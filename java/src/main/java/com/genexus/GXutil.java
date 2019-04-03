3a00a6f5e803f0076f2750e3490106bbcdee6911
gmilano@genexus.com <gmilano@genexus.com>
2019-04-03 13:02:03 -0300

    Remove GXUtilJava, the common functions are already in CommonUtil.

--- java/src/main/java/com/genexus/GXutil.java
+++ java/src/main/java/com/genexus/GXutil.java
@@ -16,10 +16,7 @@ import com.genexus.common.interfaces.SpecificImplementation;
 import com.genexus.internet.StringCollection;
 import com.genexus.platform.INativeFunctions;
 import com.genexus.platform.NativeFunctions;
-import com.genexus.util.Codecs;
-import com.genexus.util.ExpressionEvaluator;
-import com.genexus.util.GXThreadLocal;
-import com.genexus.util.IThreadLocal;
+import com.genexus.util.*;
 
 import json.org.json.JSONObject;
 
@@ -1606,4 +1603,43 @@ public final class GXutil
 	{
 		return CommonUtil.pagingSelect(select);
 	}
+
+	public static String getEncryptedSignature( String value, String key)
+	{
+		return Encryption.encrypt64(CommonUtil.getHash( com.genexus.security.web.WebSecurityHelper.StripInvalidChars(value), com.genexus.cryptography.Constants.SECURITY_HASH_ALGORITHM), key);
+	}
+	public static boolean checkEncryptedSignature( String value, String hash, String key)
+	{
+		return CommonUtil.getHash( com.genexus.security.web.WebSecurityHelper.StripInvalidChars(value), com.genexus.cryptography.Constants.SECURITY_HASH_ALGORITHM).equals(Encryption.decrypt64(hash, key));
+	}
+
+	public static String buildURLFromHttpClient(com.genexus.internet.HttpClient GXSoapHTTPClient, String serviceName, javax.xml.ws.BindingProvider bProvider)
+	{
+		if (!GXSoapHTTPClient.getProxyServerHost().equals(""))
+		{
+			bProvider.getRequestContext().put("https.proxyHost", GXSoapHTTPClient.getProxyServerHost());
+		}
+
+		if (GXSoapHTTPClient.getProxyServerPort() != 80)
+		{
+			bProvider.getRequestContext().put("https.proxyPort", GXSoapHTTPClient.getProxyServerPort());
+		}
+
+		String scheme = "http";
+		if (GXSoapHTTPClient.getSecure() == 1)
+		{
+			scheme = "https";
+		}
+		java.net.URI url = null;
+		try
+		{
+			url = new java.net.URI(scheme, null, GXSoapHTTPClient.getHost(), GXSoapHTTPClient.getPort(), GXSoapHTTPClient.getBaseURL() + serviceName, null, null);
+		}
+		catch(java.net.URISyntaxException e)
+		{
+			return "";
+		}
+		return url.toString();
+	}
+
 }
