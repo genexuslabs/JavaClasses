3604f4b5a5797c65c7ea9a74c7cf93a41b7b97c0
gmilano@genexus.com <gmilano@genexus.com>
2019-04-03 22:21:41 -0300

    Bring changes from old repository.

--- java/src/main/java/com/genexus/util/GxUtilsLoader.java
+++ java/src/main/java/com/genexus/util/GxUtilsLoader.java
@@ -1,22 +1,21 @@
 package com.genexus.util;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Hashtable;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.util.zip.*;
+import java.util.*;
+import java.io.*;
+import java.net.URL;
+import java.net.URISyntaxException;
+import java.util.jar.JarFile;
 
 public class GxUtilsLoader extends ClassLoader
 {
 	public static String GXUTILS_FILE = "GxUtils.jar";
 	private static GxUtilsLoader loader = null;
-	
+
 	public static void runDeveloperMenu(String [] arg)
 	{
 		try
-		{				
+		{
 			Class c = getClass("com.genexus.gx.deployment.developermenu");
 			c.getMethod("start", new Class[]{String[].class}).invoke(c.newInstance(), new Object[]{arg});
 		}catch(java.lang.reflect.InvocationTargetException e)
@@ -27,7 +26,7 @@ public class GxUtilsLoader extends ClassLoader
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void runServerConfig(String []fileName)
 	{
 		try
@@ -39,7 +38,7 @@ public class GxUtilsLoader extends ClassLoader
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void runClientConfig(String []fileName)
 	{
 		try
@@ -51,9 +50,9 @@ public class GxUtilsLoader extends ClassLoader
 			e.printStackTrace();
 		}
 	}
-	
+
 	///--------------------------------------------------------------------
-	
+
 	private static Class getClass(String className)throws Exception
 	{
 		try
@@ -65,8 +64,8 @@ public class GxUtilsLoader extends ClassLoader
 		}
 		return loader.loadClass(className);
 	}
-	
-	private static void setLoader() throws Exception 
+
+	private static void setLoader() throws Exception
 	{
 		if(loader == null)
 		{
@@ -75,13 +74,13 @@ public class GxUtilsLoader extends ClassLoader
 			loader = new GxUtilsLoader("GxUtils.jar");
 		}
 	}
-	
+
 	private String source;
 	private ZipFile zipFile = null;
 	private Hashtable classes = new Hashtable();
 	private int loadDepth; // Esta variable mantiene un depth de intentos de lectura del Zip
-	
-	/** El Nombre esta medio mal, porque el GXJarClassLoader obtiene las clases de un JAR o 
+
+	/** El Nombre esta medio mal, porque el GXJarClassLoader obtiene las clases de un JAR o
 	 * de una directorio base
 	 * @param location Archivo ZIP/JAR o ruta a directorio base
 	 * @see getClassLoader
@@ -89,19 +88,19 @@ public class GxUtilsLoader extends ClassLoader
 	public GxUtilsLoader(String location)
 	{
 		this.source = location.trim().toLowerCase();
-		
+
 		loadDepth = 0;
 		openJar();
 	}
-	
-	
-	public Class loadClass(String className) throws ClassNotFoundException 
+
+
+	public Class loadClass(String className) throws ClassNotFoundException
 	{
 		Class cls = (loadClass(className, true));
-		
+
 		return cls;
 	}
-	
+
 	private void openJar()
 	{
 		if(zipFile == null)
@@ -121,7 +120,7 @@ public class GxUtilsLoader extends ClassLoader
 		}
 		loadDepth ++;
 	}
-	
+
 	private void closeJar()
 	{
 		loadDepth --;
@@ -136,68 +135,68 @@ public class GxUtilsLoader extends ClassLoader
 		zipFile = null;
 
 	}
-	
-	public synchronized Class loadClass(String className, boolean resolveIt) throws ClassNotFoundException 
+
+	public synchronized Class loadClass(String className, boolean resolveIt) throws ClassNotFoundException
 	{
 		Class   result;
 		byte[]  classBytes;
 
 		// Primero vemos si es una SystemClass
-		try 
+		try
 		{
 			result = super.findSystemClass(className);
 			return result;
 		}
 		catch (Throwable e) { ; }
-		
+
 		// Ahora la busco en el cache
-		
+
 		if ((result = (Class) classes.get(className)) != null)
 			return result;
-		
+
 		classBytes = loadBytes(className);
 
-		if (classBytes == null) 
+		if (classBytes == null)
 		{
 			closeJar();
-			// OK, si nuestro classLoader NO pudo encontrar la clase, le damos una �ltima oportunidad
-			// buscando en el ParentClassLoader (no tiene por qu� haber uno)
+			// OK, si nuestro classLoader NO pudo encontrar la clase, le damos una última oportunidad
+			// buscando en el ParentClassLoader (no tiene por qué haber uno)
 			// Esto es importante cuando el esquema de classloaders es el de Java 2 (x ej, el usado en el Tomcat 4)
 			try
 			{
 				result = this.getClass().getClassLoader().loadClass(className);
 				return result;
-			}catch(Throwable e) { ; }            
+			}catch(Throwable e) { ; }
 			throw new ClassNotFoundException(className);
 		}
 		result = defineClass(classBytes, 0, classBytes.length);
-		
-		if (result == null) 
+
+		if (result == null)
 		{
 			closeJar();
 			throw new ClassFormatError();
 		}
 
-		if (resolveIt) 
+		if (resolveIt)
 			resolveClass(result);
 
 		classes.put(className, result);
 
 		return result;
 	}
-	
+
 	/** Obtiene los bytes de la clase desde el JAR o del directorio
 	 */
-	private byte[] loadBytes(String className) 
+	private byte[] loadBytes(String className)
 	{
 		if(zipFile == null)
 		{
 			throw new NoClassDefFoundError(className);
 		}
-		
+
 		byte[] result = null;
 		className = className.replace('.', '/') + ".class";
-		
+
 		try
 		{
 			ZipEntry theEntry;
@@ -210,6 +209,41 @@ public class GxUtilsLoader extends ClassLoader
 			}
 		}catch(IOException e) { ; }
 		return result;
-	}		
+	}
+	public static String getClassUniqueId(Class classType)
+	{
+		long lastModified;
+		URL resource = classType.getResource(classType.getSimpleName() + ".class");
+		try{
+			if (resource == null) {
+				throw new IllegalStateException("Failed to find class file for class: " +  classType.getName());
+			}
+			if (resource.getProtocol().equals("file")) {
+				lastModified = new File(resource.toURI()).lastModified();
+			} else if (resource.getProtocol().equals("jar")) {
+				String path = resource.getPath();
+				lastModified = new File(path.substring(5, path.indexOf("!"))).lastModified();
+			} else if (resource.getProtocol().equals("zip")) {
+				String path = resource.getPath();
+				File jarFileOnDisk = new File(path.substring(0, path.indexOf("!")));
+				try {
+					JarFile jf = new JarFile (jarFileOnDisk);
+					ZipEntry ze = jf.getEntry (path.substring(path.indexOf("!") + 2));
+					lastModified = ze.getTime ();
+				} catch (Exception e) {
+					throw new IllegalStateException("Failed to find class file in zip for class: " +  classType.getName());
+				}
+			}
+			else {
+				throw new IllegalArgumentException("Unhandled url protocol: " + resource.getProtocol() + " for class: " + classType.getName() + " resource: " + resource.toString());
+			}
+			long ticks = new Date(lastModified).getTime();
+			return "?" + new Long(ticks).toString();
+		}
+		catch(URISyntaxException e)
+		{
+			throw new IllegalArgumentException("Invalid url: " + resource.getProtocol() + " for class: " + classType.getName() + " resource: " + resource.toString());
+		}
+	}
 } 	
 
