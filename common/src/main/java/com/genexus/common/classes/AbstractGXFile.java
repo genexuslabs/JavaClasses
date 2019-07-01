300289ec1c6bd28a7f7ded7fbcd7be3a9d3364a0
iroqueta <iroqueta@genexus.com>
2019-07-01 17:26:05 -0300

    Issue: 75366 When we did the refactoring we forgot to add a lot of methods in the AbstractGXFile source.

--- common/src/main/java/com/genexus/common/classes/AbstractGXFile.java
+++ common/src/main/java/com/genexus/common/classes/AbstractGXFile.java
@@ -1,5 +1,7 @@
 package com.genexus.common.classes;
 
+import java.util.Vector;
+
 public abstract class AbstractGXFile {
 
 	public abstract String readAllText(String string);
@@ -10,4 +12,37 @@ public abstract class AbstractGXFile {
 
 	public abstract long getLength();
 
+	public abstract String getName();
+
+	public abstract java.util.Date getLastModified();
+
+	public abstract String getPath();
+
+	public abstract String getURI();
+
+	public abstract String htmlClean();
+
+	public abstract void open(String string);
+
+	public abstract void openRead(String string);
+
+	public abstract Vector readAllLines();
+
+	public abstract String readLine();
+
+	public abstract void rename(String string);
+
+	public abstract String getSeparator();
+
+	public abstract void setSource(String string);
+
+	public abstract void writeAllLines(Vector vector, String string);
+
+	public abstract void writeAllText(String string, String string1);
+
+	public abstract void writeLine(String string);
+
+	public abstract void appendAllLines(Vector vector, String string);
+
+	public abstract void appendAllText(String string, String string1);
 }
