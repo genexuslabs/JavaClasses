9e7c5f896fe03d0ad09e6792cc6a6cbdc996113c
iroqueta <46004974+iroqueta@users.noreply.github.com>
2019-07-31 13:40:41 -0300

    Issue: 75417 (#86)
    Implements a parameter in config.gx to allow close a element in xmlwriter creating a new tag in the way that it use to be before the change we made to close it in the same element tag if it is possible

--- common/src/main/java/com/genexus/xml/XMLWriter.java
+++ common/src/main/java/com/genexus/xml/XMLWriter.java
@@ -15,6 +15,8 @@ import java.math.BigDecimal;
 
 public class XMLWriter implements IXMLWriter
 {
+	private static boolean useTagToCloseElement;
+
 	private static final byte GXSUCCESS = 0;
 	private static final byte GXFAIL    = 1;
 	private static final byte GXOUTPUT_ERROR = 2;
@@ -460,7 +462,7 @@ public class XMLWriter implements IXMLWriter
 			}
 			else
 			{
-				if (printingEndElement && IsBlank(sCurrValue))
+				if (printingEndElement && IsBlank(sCurrValue) && !useTagToCloseElement)
 				{
 					out.write("/>\n");
 					printingEndElement = false;
@@ -945,6 +947,10 @@ public class XMLWriter implements IXMLWriter
 
 		return GXSUCCESS;
 	}
-	
+
+	public static void setUseTagToCloseElement(boolean useTagToCloseElementParm)
+	{
+		useTagToCloseElement = useTagToCloseElementParm;
+	}
 }
 
