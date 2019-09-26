cf423ace0a3cabf01a5db88af974c30f5cfcec46
builder <builder@genexus.com>
2019-09-26 16:00:47 +0000

    Revert branch 'MustacheUTF8' into beta

--- java/src/main/java/com/genexus/usercontrols/UserControlGenerator.java
+++ java/src/main/java/com/genexus/usercontrols/UserControlGenerator.java
@@ -1,10 +1,9 @@
 package com.genexus.usercontrols;
-import java.io.*;
+import java.io.File;
+import java.io.StringWriter;
 import java.util.Date;
 
 import com.genexus.ModelContext;
-import com.genexus.diagnostics.core.ILogger;
-import com.genexus.diagnostics.core.LogManager;
 import com.genexus.util.GXMap;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
@@ -12,7 +11,6 @@ import com.github.mustachejava.MustacheFactory;
 
 public class UserControlGenerator 
 {
-	public static final ILogger logger = LogManager.getLogger(UserControlGenerator.class);
 	private String controlType;
 	private long lastRenderTime = 0;
 	private Mustache mustache;
@@ -27,32 +25,11 @@ public class UserControlGenerator
 		File template = new File(getTemplateFile(this.controlType));
 		if (!template.exists())
 			return "";
-
+		
 		if (getTemplateDateTime() > lastRenderTime)
 		{
 			MustacheFactory mf = new DefaultMustacheFactory();
-			Reader reader = null;
-			try
-			{
-				reader = new InputStreamReader(new FileInputStream(getTemplateFile(this.controlType)), "utf-8");
-				mustache = mf.compile(reader, getTemplateFile(this.controlType));
-			}
-			catch (Exception e)
-			{
-				mustache = mf.compile(getTemplateFile(this.controlType));
-			}
-			finally
-			{
-				try
-				{
-					reader.close();
-				}
-				catch (IOException e)
-				{
-					logger.error("Failed to render UserControl ", e);
-					return "";
-				}
-			}
+			mustache = mf.compile(getTemplateFile(this.controlType));
 			lastRenderTime = new Date().getTime();
 		}
 		
