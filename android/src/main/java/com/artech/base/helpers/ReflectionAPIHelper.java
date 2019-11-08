f78487c7ab7abbc459b4ee5bd79cd81851e8a96e
Fernando Panizza <fpanizza@gmail.com>
2019-11-08 11:24:22 -0300

    catch specific exception.

--- android/src/main/java/com/artech/base/helpers/ReflectionAPIHelper.java
+++ android/src/main/java/com/artech/base/helpers/ReflectionAPIHelper.java
@@ -1,6 +1,7 @@
 package com.artech.base.helpers;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ReflectionAPIHelper {
@@ -72,10 +73,12 @@ public class ReflectionAPIHelper {
 			// instance = constructor.newInstance(-1);
 
 			return instance;
-		} catch (Exception ex) {
+		} catch (IllegalAccessException | IllegalArgumentException | InstantiationException
+			| InvocationTargetException | ExceptionInInitializerError | NoSuchMethodException ex) {
 			if (clazz != null)
 				System.err.println(
 						String.format("Exception creating instance of class '%s' by reflection.", clazz.getName())); //$NON-NLS-1$
+			System.err.println(ex.getMessage()); // $NON-NLS-1$
 			return null;
 		}
 	}
@@ -96,7 +99,7 @@ public class ReflectionAPIHelper {
 			}
 			Method method = clazz.getMethod(name, paramsTypes);
 			return method;
-		} catch (Exception ex) {
+		} catch (NoSuchMethodException | SecurityException ex) {
 			if (clazz != null) {
 				for (Method method : clazz.getMethods()) {
 					if (name.equals(method.getName())) {
@@ -122,12 +125,12 @@ public class ReflectionAPIHelper {
 	}
 
 	public static Object executeMethod(Object instance, Method method, Object[] params) {
-		// intance can be null., params can be null.
+		// instance can be null., params can be null.
 		try {
 			return method.invoke(instance, params);
-		} catch (Exception e) {
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | ExceptionInInitializerError ex) {
 			System.err.println(String.format("Exception executing method '%s' by reflection.", method.getName())); //$NON-NLS-1$
-			System.err.println(e.getMessage()); // $NON-NLS-1$
+			System.err.println(ex.getMessage()); // $NON-NLS-1$
 		}
 		return null;
 
