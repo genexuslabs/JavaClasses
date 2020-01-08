e75c00de42435688f1cfe05eb058dc06f644f3fb
iroqueta <46004974+iroqueta@users.noreply.github.com>
2020-01-08 14:12:25 -0300

    Issue: 75112 (#174)
    Get Build-Label information from manifest.mf to get the gxclassr version only if debug info is enabled.

--- java/src/main/java/com/genexus/webpanels/gxver.java
+++ java/src/main/java/com/genexus/webpanels/gxver.java
@@ -1,5 +1,7 @@
 package com.genexus.webpanels;
 
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -10,14 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public final class gxver extends HttpServlet 
 {
+	private static Logger log = org.apache.logging.log4j.LogManager.getLogger(gxver.class);
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
     {
-        response.setContentType("text/html");
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        writer.println("<body>");
-        writer.println("<b>Running GeneXus Runtime Classes Version " + com.genexus.Version.getFullVersion() + "</body>");
-        writer.println("</body>");
-        writer.println("</html>");
+    	if (log.isInfoEnabled()) {
+			response.setContentType("text/html");
+			PrintWriter writer = response.getWriter();
+			writer.println("<html>");
+			writer.println("<body>");
+			writer.println("<b>Running GeneXus Runtime Classes Version " + com.genexus.Version.getFullVersion() + "</body>");
+			writer.println("</body>");
+			writer.println("</html>");
+		}
+    	else
+		{
+			response.setStatus(404);
+		}
     }
 }
