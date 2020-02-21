cd88b93d8440801692177351a6a5ce583dcc5393
Fernando Panizza <fpanizza@gmail.com>
2020-02-21 11:22:14 -0300

    fix in getRelativeUrl in Android. Return existing path if not have servletEnginePath

--- android/src/main/java/com/genexus/specific/android/GXutil.java
+++ android/src/main/java/com/genexus/specific/android/GXutil.java
@@ -109,7 +109,11 @@ public class GXutil implements IExtensionGXutil {
 
 	@Override
 	public String getRelativeURL(String path) {
-		return com.genexus.CommonUtil.getRelativeBlobFile(path);
+		String servletEnginePath = com.genexus.ApplicationContext.getInstance().getServletEngineDefaultPath();
+		if (servletEnginePath!=null && servletEnginePath.length()>0)
+			return com.genexus.CommonUtil.getRelativeBlobFile(path);
+		else
+			return path;
 	}
 
 	@Override
