ddc1b4c6201b18d946c118bd103a19a671942aa5
claudia <cmurialdo@gmail.com>
2020-06-04 10:28:16 -0300

    Fix variables names following java naming conventions

--- java/src/main/java/com/genexus/webpanels/gridstate/GridStateInputValuesItem.java
+++ java/src/main/java/com/genexus/webpanels/gridstate/GridStateInputValuesItem.java
@@ -1,15 +1,18 @@
 package com.genexus.webpanels.gridstate ;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class GridStateInputValuesItem {
 	public GridStateInputValuesItem(String name, String value) {
-		Name = name;
-		Value = value;
+		this.name = name;
+		this.value = value;
 	}
 
 	public GridStateInputValuesItem() {
 	}
-
-	protected String Name;
-	protected String Value;
+	@JsonProperty("Name")
+	protected String name;
+	@JsonProperty("Value")
+	protected String value;
 }
 
