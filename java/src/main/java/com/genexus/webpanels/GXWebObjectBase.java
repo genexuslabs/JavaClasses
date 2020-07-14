8d7ae10a8accc20397cffccb2568b6e478a487e2
Gonzalo <gonzalogallotti@gmail.com>
2020-07-14 18:45:13 -0300

    Remove BoxImplementation Add Files removed by CherryPick.

--- java/src/main/java/com/genexus/webpanels/GXWebObjectBase.java
+++ java/src/main/java/com/genexus/webpanels/GXWebObjectBase.java
@@ -354,10 +354,6 @@ public abstract class GXWebObjectBase implements IErrorHandler, GXInternetConsta
 	public void submitReorg(int id, Object [] submitParms) throws SQLException{
 	}
 
-	protected String formatLink(String jumpURL, String[] parm1, String[] parm2) {
-		return formatLink(jumpURL);
-	}
-
 	protected String formatLink(String jumpURL)
 	{
 		return formatLink(formatLink1(jumpURL), new String[]{});
