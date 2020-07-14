b1ec1f30a627d4e372fe627bcb8f3fa1a40c8d88
Gonzalo <gonzalogallotti@gmail.com>
2020-07-14 17:59:44 -0300

    Merge from master

--- gxexternalproviders/src/main/java/com/genexus/db/driver/ExternalProviderS3.java
+++ gxexternalproviders/src/main/java/com/genexus/db/driver/ExternalProviderS3.java
@@ -28,6 +28,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
