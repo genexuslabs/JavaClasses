cf52dc387aaaaa0f4357d8f37af3d0514ae692f3
Gonzalo <gonzalogallotti@gmail.com>
2020-08-20 10:04:34 -0300

    Do not PAD whith white spaces when Character is Null (#260)

--- java/src/main/java/com/genexus/db/driver/GXResultSet.java
+++ java/src/main/java/com/genexus/db/driver/GXResultSet.java
@@ -290,7 +290,7 @@ public final class GXResultSet implements ResultSet, com.genexus.db.IFieldGetter
 		{
 			value = result.getString(columnIndex);
 			if (result.wasNull() || value == null)
-				value = CommonUtil.replicate(" ", length);
+				value = "";
 			else
 				value = String.format(String.format("%%-%ds", length), value);
 		}
