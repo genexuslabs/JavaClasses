c238d9a566abdf4f41fd8c9b0fd91d707936e89f
Gonzalo <gonzalogallotti@gmail.com>
2020-09-17 15:26:59 -0300

    Delete push.bat

--- push.bat
+++
@@ -1,6 +0,0 @@
-
-SET lib=C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps\TestStoreJavaSqlIBM\WEB-INF\lib
-echo %lib%
-copy "java\target\gxclassR.jar" "%lib%\gxclassR.jar" /Y
-copy "common\target\gxcommon.jar" "%lib%\gxcommon.jar" /Y
-copy "gxexternalpobject_idroviders\target\gxexternalproviders.jar" "%lib%\gxexternalproviders.jar" /Y
