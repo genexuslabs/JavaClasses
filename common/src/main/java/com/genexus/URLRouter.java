27da90eb1f24952ef0c7a61bee2a257a3b4f5afa
iroqueta <iroqueta@genexus.com>
2020-09-28 17:48:12 -0300

    Dynamic link with absolute URL and parameters was not working, the URL was returned without parameters Issue:85568

--- common/src/main/java/com/genexus/URLRouter.java
+++ common/src/main/java/com/genexus/URLRouter.java
@@ -27,7 +27,7 @@ public class URLRouter
 	public static String getURLRoute(boolean useNamedParameters, String key, String[] parms, String[] parmsName, String contextPath)
 	{
 		if (com.genexus.CommonUtil.isAbsoluteURL(key) || key.startsWith("/")) {
-			return key;
+			return ((parms.length > 0)? key + "?" + String.join(",", parms): key);
 		}
 
 		if	(routerList == null)
