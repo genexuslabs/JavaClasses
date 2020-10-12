488438b88883fca4c09a9dafc02448b93ac076fc
claudiamurialdo <33756655+claudiamurialdo@users.noreply.github.com>
2020-10-12 15:45:06 -0300

    Fix condition for absolute urls (#288)
    * Fix condition for absolute urls
    
    * Remove unnecessary formatLink1 call.
    
    * Link of files with absoluteURL was not working
    Issue:85852
    
    * A refactoring was made to avoid duplicate code
    Issue:85852
    
    * A refactoring was made to avoid duplicate code
    Issue:85852
    
    Co-authored-by: claudia <cmurialdo@gmail.com>
    Co-authored-by: iroqueta <iroqueta@genexus.com>

--- java/src/main/java/com/genexus/webpanels/GXWebObjectBase.java
+++ java/src/main/java/com/genexus/webpanels/GXWebObjectBase.java
@@ -356,7 +356,7 @@ public abstract class GXWebObjectBase implements IErrorHandler, GXInternetConsta
 
 	protected String formatLink(String jumpURL)
 	{
-		return formatLink(formatLink1(jumpURL), new String[]{});
+		return formatLink(jumpURL, new String[]{});
 	}
 
 	protected String formatLink(String jumpURL, String[] parms)
@@ -366,56 +366,7 @@ public abstract class GXWebObjectBase implements IErrorHandler, GXInternetConsta
 
 	protected String formatLink(String jumpURL, String[] parms, String[] parmsName)
 	{
-		return URLRouter.getURLRoute(formatLink1(jumpURL), parms, parmsName, httpContext.getRequest().getContextPath());
-	}
-
-	private String formatLink1(String jumpURL)
-	{
-		String lowURL = CommonUtil.lower(jumpURL);
-		if (jumpURL.indexOf("?") != -1)
-		{
-				lowURL = jumpURL.substring(0, jumpURL.indexOf("?")).toLowerCase() + jumpURL.substring(jumpURL.indexOf("?"));
-		}
-		String packageName = context.getPackageName();
-
-		if (!packageName.equals("") && lowURL.startsWith(packageName))
-		{
-			return lowURL;
-		}
-		else
-		{
-			try
-			{
-				if (!packageName.equals(""))
-					packageName += ".";
-				getClass().getClassLoader().loadClass(packageName + lowURL);
-				return packageName + lowURL;
-			}
-			catch(java.lang.ClassNotFoundException e)
-			{
-			}
-		}
-
-		if (lowURL.split("\\.").length == 2 && !(!packageName.equals("") && lowURL.startsWith(packageName)) && !lowURL.startsWith("file:") && !lowURL.startsWith("http:") && !lowURL.startsWith("https:") && !lowURL.startsWith("/"))
-		{
-			if (lowURL.indexOf("?") == -1 || lowURL.indexOf("?") > lowURL.indexOf("."))
-			{
-				return httpContext.getRequest().getContextPath() + "/" + jumpURL;
-			}
-		}
-
-		if	(packageName.endsWith(".") && packageName.trim().equals(""))
-		{
-			packageName += ".";
-		}
-
-		// Convierte el 'call', agregandole el package.
-		if	(GXWebPanel.isStaticGeneration && (lowURL.startsWith("http:" + packageName + "h") || lowURL.startsWith("https:" + packageName + "h")))
-		{
-			return  WebUtils.getDynURL() + jumpURL.substring(lowURL.indexOf(':') + 1);
-		}
-
-		return jumpURL;
+		return URLRouter.getURLRoute(jumpURL, parms, parmsName, httpContext.getRequest().getContextPath(), context.getPackageName());
 	}
 
 	public String getPgmname()
