3aec1a9f0da698441d2bb400fd7f54494e9fc433
builder <builder@genexus.com>
2020-11-05 17:46:29 +0000

    Cherry pick branch 'genexuslabs:apifix' into beta

--- java/src/main/java/com/genexus/filters/APIObjectFilter.java
+++ java/src/main/java/com/genexus/filters/APIObjectFilter.java
@@ -17,7 +17,6 @@ import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
