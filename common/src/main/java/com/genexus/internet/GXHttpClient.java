acc025f81756eb074f3c187c08ef382ebde4a679
MartinVigliarolo <martin.vigliarolo@gmail.com>
2020-11-06 17:27:42 -0300

    Primer commit con interfaz implementada. Falta implementacion de metodos con libreria HttpClient Java e implementacion de clase que obtiene clase correcta de HttpClient
    (cherry picked from commit 144e353dccccd4195f0f77d2a492823335e641ba)

---
+++ common/src/main/java/com/genexus/internet/GXHttpClient.java
@@ -0,0 +1,23 @@
+//package com.genexus.internet;
+//
+//import JavaClasses.java.src.main.java.com.genexus.internet.HttpClientJavaLib;
+//
+//public class GXHttpClient {
+//	private IHttpClient session;
+//
+//
+//	public GXHttpClient() {
+//		try
+//		{
+//			//Class c = Class.forName("javax.mail.Session");
+//			session = new HttpClientJavaLib();
+//		}
+//		catch(Throwable e)
+//		{
+//			session = new HttpClient();
+//		}
+//
+//		// Seteo inicial de los atributos que haya que declarar en esta clase
+//
+//	}
+//}
---
+++ common/src/main/java/com/genexus/internet/IHttpClient.java
@@ -0,0 +1,54 @@
+package com.genexus.internet;
+
+import java.io.*;
+
+public interface IHttpClient {
+	public byte getBasic();
+	public byte getDigest();
+	public byte getNtlm();
+	public short getErrCode();
+	public String getErrDescription();
+	public void setProxyServerHost(String host);
+	public String getProxyServerHost();
+	public void setProxyServerPort(long port);
+	public short getProxyServerPort();
+	public void setIncludeCookies(boolean value);
+	public boolean getIncludeCookies();
+	public void setUrl(String url);
+	public void setURL(String stringURL);
+	public void setHost(String host);
+	public String getHost();
+	public void setWSDLURL(String WSDLURL);
+	public void setBaseURL(String baseURL);
+	public String getWSDLURL();
+	public String getBaseURL();
+	public void setPort(int port);
+	public int getPort();
+	public byte getSecure();
+	public void setSecure(int secure);
+	public void setTimeout(int timeout);
+	public int getTimeout();
+	public void setTcpNoDelay(boolean tcpNoDelay);
+	public void addAuthentication(int type, String realm, String name, String value);
+	public void addProxyAuthentication(int type, String realm, String name, String value);
+	public void addCertificate(String fileName);
+	public void addHeader(String name, String value);
+	public void addVariable(String name, String value);
+	public void addBytes(byte[] value);
+	public void addString(String value);
+	public void addFile(String fileName);
+	public void addFile(String fileName, String varName);
+	public void addStringWriter(StringWriter writer, StringBuffer encoding);
+	public void execute(String method, String url);
+	public int getStatusCode();
+	public String getReasonLine();
+	public void getHeader(String name, long[] value);
+	public String getHeader(String name);
+	public void getHeader(String name, String[] value);
+	public void getHeader(String name, java.util.Date[] value);
+	public void getHeader(String name, double[] value);
+	public InputStream getInputStream() throws IOException;
+	public String getString();
+	public void toFile(String fileName);
+	public void cleanup();
+}
