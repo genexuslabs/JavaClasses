14d4f5882fb68afdd9114e570eeaf187af5ff4f0
builder <builder@genexus.com>
2020-11-11 16:32:10 +0000

    Cherry pick branch 'genexuslabs:build-refactoring' into beta

--- .github/updatePOMVersion.sh
+++ .github/updatePOMVersion.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-GIT_HASH=$(git rev-parse HEAD)
-if ! [ "$(git symbolic-ref --short HEAD)" = "beta" ]; then
-  echo "## Is MASTER or UPGRADE  branch, use pom.xml version"
-else
-  currentversion=$(sed -n '/<version>.*-SNAPSHOT<\/version>/ s/<version>\(.*\)-SNAPSHOT<\/version>/\1/p' pom.xml)
-  devversionname="$currentversion-${GIT_HASH:0:7}-SNAPSHOT"
-  echo "## Is DEVELOPER branch, update pom.xml version to: $devversionname"
-  mvn -B versions:set -DgenerateBackupPoms=false -DnewVersion="$devversionname"
-fi
-
 scmversion="<vcm_hash>$GIT_HASH</vcm_hash>"
 scmv=$(echo $scmversion | sed 's/\//\\\//g')
 sed -i "/<\/properties>/ s/.*/${scmv}\n&/" pom.xml
