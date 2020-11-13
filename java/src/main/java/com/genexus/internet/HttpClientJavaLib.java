211684d59263fcc14449422cba2b2885a8e27ada
MartinVigliarolo <martin.vigliarolo@gmail.com>
2020-11-13 14:07:21 -0300

    Commit previo a la implementacion usando libreria HttpClient

--- java/src/main/java/com/genexus/internet/HttpClientJavaLib.java
+++ java/src/main/java/com/genexus/internet/HttpClientJavaLib.java
@@ -4,8 +4,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import org.apache.http.client.*;
+import HTTPClient.*;
 
 public class HttpClientJavaLib extends GXHttpClient {
+	private int statusCode;
+	private String reasonLine;
+
+
+	private void resetExecParams() {
+		this.statusCode = 0;
+		this.reasonLine = "";
+		resetErrors();
+	}
+
 
 	public void addAuthentication(int type, String realm, String name, String value) {
 	}
@@ -13,11 +24,40 @@ public class HttpClientJavaLib extends GXHttpClient {
 	public void addProxyAuthentication(int type, String realm, String name, String value) {
 	}
 
-
-	public void addHeader(String name, String value) {
-	}
-
 	public void execute(String method, String url) {
+		resetExecParams();
+
+
+		url = getURLValid(url);
+		if (getSecure() == 1)
+			url = "https://" + getHost();
+		else
+			url = "http://" + getHost();
+
+
+
+		// HACER ACA LA PARTE DEL AGREGADO DE LOS HEADERS DE AUTH
+
+
+		// Al final de cada ejecucion se setean los atributos
+
+		if (method.equalsIgnoreCase("GET")) {
+
+
+		} else if (method.equalsIgnoreCase("POST")) {
+
+		} else if (method.equalsIgnoreCase("PUT")) {
+
+		} else if (method.equalsIgnoreCase("DELETE")) {
+
+		} else {
+			// VER COMO TRATAR ACA LOS OTROS METODOS QUE PUEDEN VENIR
+		}
+
+
+
+		resetState();
+
 	}
 
 	public int getStatusCode() {
