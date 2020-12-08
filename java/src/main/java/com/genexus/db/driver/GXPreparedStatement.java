86429fd413d00b46683cc4c7229e2bbc4a23ab7b
claudia <cmurialdo@gmail.com>
2020-12-08 18:58:55 -0300

    Reset recordCount when cursor is initialized

--- java/src/main/java/com/genexus/db/driver/GXPreparedStatement.java
+++ java/src/main/java/com/genexus/db/driver/GXPreparedStatement.java
@@ -1701,6 +1701,7 @@ public class GXPreparedStatement extends GXStatement implements PreparedStatemen
             batchRecords = new Object[size];
             batchSize = size;
             batchStmt = true;
+			resetRecordCount();
         }
 
         public Object[] getBatchRecords(){
