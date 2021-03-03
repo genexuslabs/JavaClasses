0763133055ee1a80f433d0ff9300ca1edb1a539b
Gonzalo <gonzalogallotti@gmail.com>
2021-03-03 13:46:44 -0300

    Send Cache Headers in SPA Response Redirects

--- java/src/main/java/com/genexus/webpanels/HttpContextWeb.java
+++ java/src/main/java/com/genexus/webpanels/HttpContextWeb.java
@@ -1275,6 +1275,7 @@ public class HttpContextWeb extends HttpContext {
 			if (isSpaRequest(true)) {
 				pushUrlSessionStorage();
 				getResponse().setHeader(GX_SPA_REDIRECT_URL, url + popLvlParm);
+				sendCacheHeaders();
 			} else {
 				redirect_http(url + popLvlParm);
 			}
