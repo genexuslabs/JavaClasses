c192307e91f8dc81755846b5157d7c2cef2779b8
José Echagüe <jechague@genexus.com>
2021-05-20 11:39:16 -0300

    Fix executeUdp method search

--- gxmaps/src/main/java/com/genexus/gxmaps/GXMaps.java
+++ gxmaps/src/main/java/com/genexus/gxmaps/GXMaps.java
@@ -106,9 +106,10 @@ public class GXMaps {
 
     private static final String DIRECTIONS_SERVICE_INTERNAL_PROCEDURE_CLASS_NAME = "com.genexuscore.genexus.common.googlemapsdirectionsserviceinternal";
 
-    public static GXXMLSerializable calculateDirections(Object DirectionsParametersSDT) 
+    public static GXXMLSerializable calculateDirections(Object DirectionsParametersSDTArray) 
     {
         try {
+            Object DirectionsParametersSDT = ((Object[]) DirectionsParametersSDTArray)[0];
             Class<?> DirectionsServiceProcedureClass = Class.forName(DIRECTIONS_SERVICE_INTERNAL_PROCEDURE_CLASS_NAME);
             Object DirectionsServiceProcedure = DirectionsServiceProcedureClass.getDeclaredConstructor(Integer.TYPE).newInstance(-1);
 
