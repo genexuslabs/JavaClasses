a9ac59b83ed2f0868abcd0c00b3a5d119dfe0e75
Gonzalo <gonzalogallotti@gmail.com>
2021-06-11 00:24:05 -0300

    GXExternal File ACL was not beign correctly set in Storage Provider methods. Specially needed for Azure where Public and private containers are isolated

--- java/src/main/java/com/genexus/util/GXStorageProvider.java
+++ java/src/main/java/com/genexus/util/GXStorageProvider.java
@@ -12,144 +12,150 @@ import com.genexus.db.driver.ResourceAccessControlList;
 
 public class GXStorageProvider {
 
-    protected ExternalProvider provider;
+	protected ExternalProvider provider;
 
-    public GXStorageProvider() {
-        provider = Application.getExternalProviderAPI();
-    }
+	public GXStorageProvider() {
+		provider = Application.getExternalProviderAPI();
+	}
 
-    public GXStorageProvider(GXStorageProvider other) {
-        provider = other.provider;
-    }
+	public GXStorageProvider(GXStorageProvider other) {
+		provider = other.provider;
+	}
 
-    void validProvider() throws Exception {
-        if (provider == null) {
-            throw new Exception("External provider not found");
-        }
-    }
+	void validProvider() throws Exception {
+		if (provider == null) {
+			throw new Exception("External provider not found");
+		}
+	}
 
-    public boolean upload(String filefullpath, String storageobjectfullname, GXFile uploadedFile, GXBaseCollection<SdtMessages_Message> messages) {
-        try {
-            validProvider();
-            if (storageobjectfullname.isEmpty()) {
-                storageobjectfullname = new File(filefullpath).getName();
-            }
-            String url = provider.upload(filefullpath, storageobjectfullname, ResourceAccessControlList.PublicRead);
-            if (uploadedFile != null) {
-                uploadedFile.setFileInfo(new GXExternalFileInfo(storageobjectfullname, url, provider));
-            }
-            return true;
-        } catch (Exception ex) {
-            storageMessages(ex, messages);
-            return false;
-        }
-    }
+	public boolean upload(String filefullpath, String storageobjectfullname, GXFile uploadedFile, GXBaseCollection<SdtMessages_Message> messages) {
+		try {
+			validProvider();
+			if (storageobjectfullname.isEmpty()) {
+				storageobjectfullname = new File(filefullpath).getName();
+			}
+			ResourceAccessControlList acl = ResourceAccessControlList.PublicRead;
+			String url = provider.upload(filefullpath, storageobjectfullname, acl);
+			if (uploadedFile != null) {
+				uploadedFile.setFileInfo(new GXExternalFileInfo(storageobjectfullname, url, provider, acl));
+			}
+			return true;
+		} catch (Exception ex) {
+			storageMessages(ex, messages);
+			return false;
+		}
+	}
 
-    public boolean uploadPrivate(String filefullpath, String storageobjectfullname, GXFile uploadedFile, GXBaseCollection<SdtMessages_Message> messages) {
-        try {
-            validProvider();
-            if (storageobjectfullname.isEmpty()) {
-                storageobjectfullname = new File(filefullpath).getName();
-            }
-            String url = provider.upload(filefullpath, storageobjectfullname, ResourceAccessControlList.Private);
-            if (uploadedFile != null) {
-                uploadedFile.setFileInfo(new GXExternalFileInfo(storageobjectfullname, url, provider));
-            }
-            return true;
-        } catch (Exception ex) {
-            storageMessages(ex, messages);
-            return false;
-        }
-    }
+	public boolean uploadPrivate(String filefullpath, String storageobjectfullname, GXFile uploadedFile, GXBaseCollection<SdtMessages_Message> messages) {
+		try {
+			validProvider();
+			ResourceAccessControlList acl = ResourceAccessControlList.Private;
+			if (storageobjectfullname.isEmpty()) {
+				storageobjectfullname = new File(filefullpath).getName();
+			}
+			String url = provider.upload(filefullpath, storageobjectfullname, acl);
+			if (uploadedFile != null) {
+				uploadedFile.setFileInfo(new GXExternalFileInfo(storageobjectfullname, url, provider, acl));
+			}
+			return true;
+		} catch (Exception ex) {
+			storageMessages(ex, messages);
+			return false;
+		}
+	}
 
-    public boolean download(String storageobjectfullname, GXFile localFile, GXBaseCollection<SdtMessages_Message> messages) {
-        try {
-            validProvider();
-            String destFileName;
-            destFileName = localFile.getAbsolutePath();
-            provider.download(storageobjectfullname, destFileName, ResourceAccessControlList.PublicRead);
-            return true;
-        } catch (Exception ex) {
-            storageMessages(ex, messages);
-            return false;
-        }
-    }
+	public boolean download(String storageobjectfullname, GXFile localFile, GXBaseCollection<SdtMessages_Message> messages) {
+		try {
+			validProvider();
+			String destFileName;
+			destFileName = localFile.getAbsolutePath();
+			provider.download(storageobjectfullname, destFileName, ResourceAccessControlList.PublicRead);
+			return true;
+		} catch (Exception ex) {
+			storageMessages(ex, messages);
+			return false;
+		}
+	}
 
-    public boolean downloadPrivate(String storageobjectfullname, GXFile localFile, GXBaseCollection<SdtMessages_Message> messages) {
-        try {
-            validProvider();
-            String destFileName;
-            destFileName = localFile.getAbsolutePath();
-            provider.download(storageobjectfullname, destFileName, ResourceAccessControlList.Private);
-            return true;
-        } catch (Exception ex) {
-            storageMessages(ex, messages);
-            return false;
-        }
-    }
+	public boolean downloadPrivate(String storageobjectfullname, GXFile localFile, GXBaseCollection<SdtMessages_Message> messages) {
+		try {
+			validProvider();
+			String destFileName;
+			destFileName = localFile.getAbsolutePath();
+			provider.download(storageobjectfullname, destFileName, ResourceAccessControlList.Private);
+			return true;
+		} catch (Exception ex) {
+			storageMessages(ex, messages);
+			return false;
+		}
+	}
 
-    public boolean get(String storageobjectfullname, GXFile externalFile, GXBaseCollection<SdtMessages_Message> messages) {
-        try {
-            validProvider();
-            String url = provider.get(storageobjectfullname, ResourceAccessControlList.PublicRead, 0);
-            if (url.isEmpty()) {
-                GXutil.ErrorToMessages("Get Error", "File doesn't exist", messages);
-                return false;
-            }
-            externalFile.setFileInfo(new GXExternalFileInfo(storageobjectfullname, url, provider));
-            return true;
-        } catch (Exception ex) {
-            storageMessages(ex, messages);
-            return false;
-        }
+	public boolean get(String storageobjectfullname, GXFile externalFile, GXBaseCollection<SdtMessages_Message> messages) {
+		try {
+			validProvider();
+			ResourceAccessControlList acl = ResourceAccessControlList.PublicRead;
+			String url = provider.get(storageobjectfullname, acl, 0);
+			if (url.isEmpty()) {
+				GXutil.ErrorToMessages("Get Error", "File doesn't exist", messages);
+				return false;
+			}
+			externalFile.setFileInfo(new GXExternalFileInfo(storageobjectfullname, url, provider, acl));
+			return true;
+		} catch (Exception ex) {
+			storageMessages(ex, messages);
+			return false;
+		}
 
-    }
+	}
 
-    public boolean getPrivate(String storageobjectfullname, GXFile externalFile, int expirationMinutes, GXBaseCollection<SdtMessages_Message> messages) {
-        try {
-            validProvider();
-            String url = provider.get(storageobjectfullname, ResourceAccessControlList.Private, expirationMinutes);
-            if (url.isEmpty()) {
-                GXutil.ErrorToMessages("Get Error", "File doesn't exist", messages);
-                return false;
-            }
-            externalFile.setFileInfo(new GXExternalFileInfo(storageobjectfullname, url, provider));
-            return true;
-        } catch (Exception ex) {
-            storageMessages(ex, messages);
-            return false;
-        }
+	public boolean getPrivate(String storageobjectfullname, GXFile externalFile, int expirationMinutes, GXBaseCollection<SdtMessages_Message> messages) {
+		try {
+			validProvider();
+			ResourceAccessControlList acl = ResourceAccessControlList.Private;
+			String url = provider.get(storageobjectfullname, acl, expirationMinutes);
+			if (url.isEmpty()) {
+				GXutil.ErrorToMessages("Get Error", "File doesn't exist", messages);
+				return false;
+			}
+			externalFile.setFileInfo(new GXExternalFileInfo(storageobjectfullname, url, provider, acl));
+			return true;
+		} catch (Exception ex) {
+			storageMessages(ex, messages);
+			return false;
+		}
 
-    }
+	}
 
-    public boolean getDirectory(String directoryFullName, GXDirectory externalDirectory, GXBaseCollection<SdtMessages_Message> messages) {
-        try {
-            validProvider();
-            String url = provider.getDirectory(directoryFullName);
-            if (url.isEmpty()) {
-                GXutil.ErrorToMessages("Get Error", "Directory doesn't exist", messages);
-                return false;
-            }
-            externalDirectory.setDirectoryInfo(new GXExternalFileInfo(directoryFullName, url, provider, ResourceAccessControlList.Default));
-            return true;
-        } catch (Exception ex) {
-            storageMessages(ex, messages);
-            return false;
-        }
+	public boolean getDirectory(String directoryFullName, GXDirectory externalDirectory, GXBaseCollection<SdtMessages_Message> messages) {
+		try {
+			validProvider();
+			String url = provider.getDirectory(directoryFullName);
+			if (url.isEmpty()) {
+				GXutil.ErrorToMessages("Get Error", "Directory doesn't exist", messages);
+				return false;
+			}
+			GXExternalFileInfo directoryInfo = new GXExternalFileInfo(directoryFullName, url, provider, ResourceAccessControlList.Private);
+			directoryInfo.setIsFile(false);
+			externalDirectory.setDirectoryInfo(directoryInfo);
+			return true;
+		} catch (Exception ex) {
+			storageMessages(ex, messages);
+			return false;
+		}
 
-    }
+	}
 
-    protected void storageMessages(Exception ex, GXBaseCollection<SdtMessages_Message> messages) {
-        if (messages != null && ex != null) {
-            StructSdtMessages_Message struct = new StructSdtMessages_Message();
-            if (provider!=null && provider.getMessageFromException(ex, struct)) {
-                struct.setDescription(ex.getMessage());
-                struct.setType((byte) 1); //error
-                SdtMessages_Message msg = new SdtMessages_Message(struct);
-                messages.add(msg);
-            } else {
-                GXutil.ErrorToMessages("Storage Error", ex.getClass()+" " +ex.getMessage(), messages);
-            }
-        }
-    }
+	protected void storageMessages(Exception ex, GXBaseCollection<SdtMessages_Message> messages) {
+		if (messages != null && ex != null) {
+			StructSdtMessages_Message struct = new StructSdtMessages_Message();
+			if (provider!=null && provider.getMessageFromException(ex, struct)) {
+				struct.setDescription(ex.getMessage());
+				struct.setType((byte) 1); //error
+				SdtMessages_Message msg = new SdtMessages_Message(struct);
+				messages.add(msg);
+			} else {
+				GXutil.ErrorToMessages("Storage Error", ex.getClass()+" " +ex.getMessage(), messages);
+			}
+		}
+	}
 }
