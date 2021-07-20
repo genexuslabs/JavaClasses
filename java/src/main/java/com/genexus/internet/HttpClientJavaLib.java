db287e5872ab7ed11c350f4002beedcdb1be46b9
MartinVigliarolo <43487398+MartinVigliarolo@users.noreply.github.com>
2021-07-20 11:50:54 -0300

    [HttpClientNewImpl] Read more than ones from response stream corrected (#400)
    * [HttpClientNewImpl] Read more than ones from response stream corrected
    
    * Correction from sign getEntity to setEntity
    
    Co-authored-by: Martin Vigliarolo <MVigliarolo@Artech.local>

--- java/src/main/java/com/genexus/internet/HttpClientJavaLib.java
+++ java/src/main/java/com/genexus/internet/HttpClientJavaLib.java
@@ -7,17 +7,16 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+
+import HTTPClient.ParseException;
 import com.genexus.servlet.http.ICookie;
 import com.genexus.util.IniFile;
-import org.apache.http.HttpResponse;
+import org.apache.http.*;
 import com.genexus.CommonUtil;
 import com.genexus.specific.java.*;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.protocol.HttpContext;
-import org.apache.http.Header;
-import org.apache.http.HeaderElement;
-import org.apache.http.HeaderElementIterator;
-import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthSchemeProvider;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.NTCredentials;
@@ -114,6 +113,7 @@ public class HttpClientJavaLib extends GXHttpClient {
 	private CredentialsProvider credentialsProvider = null;
 	private RequestConfig reqConfig = null;		// Atributo usado en la ejecucion del metodo (por ejemplo, httpGet, httpPost)
 	private CookieStore cookies;
+	private ByteArrayEntity entity = null;	// Para mantener el stream luego de cerrada la conexion en la lectura de la response
 	private static IniFile clientCfg = new com.genexus.ModelContext(com.genexus.ModelContext.getModelContextPackageClass()).getPreferences().getIniFile();
 
 
@@ -123,6 +123,7 @@ public class HttpClientJavaLib extends GXHttpClient {
 		resetErrorsAndConnParams();
 		setErrCode(0);
 		setErrDescription("");
+		entity = null;
 	}
 
 
@@ -564,9 +565,10 @@ public class HttpClientJavaLib extends GXHttpClient {
 	}
 
 	public InputStream getInputStream() throws IOException {
-		if (response != null)
-			return response.getEntity().getContent();
-		else
+		if (response != null) {
+			this.setEntity();
+			return entity.getContent();
+		} else
 			return null;
 	}
 
@@ -583,11 +585,17 @@ public class HttpClientJavaLib extends GXHttpClient {
 		}
 	}
 
+	private void setEntity() throws IOException {
+		if (entity == null)
+			entity = new ByteArrayEntity(EntityUtils.toByteArray(response.getEntity()));
+	}
+
 	public String getString() {
 		if (response == null)
 			return "";
 		try {
-			String res = EntityUtils.toString(response.getEntity(), "UTF-8");
+			this.setEntity();
+			String res = EntityUtils.toString(entity, "UTF-8");
 			return res;
 		} catch (IOException e) {
 			setExceptionsCatch(e);
@@ -600,7 +608,8 @@ public class HttpClientJavaLib extends GXHttpClient {
 		if (response == null)
 			return;
 		try {
-			CommonUtil.InputStreamToFile(response.getEntity().getContent(), fileName);
+			this.setEntity();
+			CommonUtil.InputStreamToFile(entity.getContent(), fileName);
 		} catch (IOException e) {
 			setExceptionsCatch(e);
 		}
