791c7de841b9072eb62aa4ea3d1d0ce0a0391aa6
Martin Vigliarolo <MVigliarolo@Artech.local>
2021-09-03 13:32:51 -0300

    Cherry pick branch 'genexuslabs:Issue91103' into beta

--- java/src/main/java/com/genexus/internet/HttpClientJavaLib.java
+++ java/src/main/java/com/genexus/internet/HttpClientJavaLib.java
@@ -250,7 +250,7 @@ public class HttpClientJavaLib extends GXHttpClient {
 	public void execute(String method, String url) {
 		resetExecParams();
 
-		url = getURLValid(url);		// Funcion genera parte del path en adelante de la URL
+		url = getURLValid(url).trim();		// Funcion genera parte del path en adelante de la URL
 
 		try {
 			CookieStore cookiesToSend = null;
