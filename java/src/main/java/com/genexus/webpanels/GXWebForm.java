fcf61b3456c848e6e90060386ce22f419d5bb91a
dmendez <dmendez@genexus.com>
2021-10-14 16:41:36 -0300

    Revert "Fix issue 90951"
    This reverts commit 14b2a983e9c8c1a2897cbb2d6d155518185777c7.

--- java/src/main/java/com/genexus/webpanels/GXWebForm.java
+++ java/src/main/java/com/genexus/webpanels/GXWebForm.java
@@ -214,6 +214,7 @@ public int getTop()
 
  public static void addResponsiveMetaHeaders(com.genexus.webpanels.HTMLChoice meta)
  {
+    tryAddMetaHeader(meta, "viewport", "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no");
     tryAddMetaHeader(meta, "apple-mobile-web-app-capable", "yes");
  }
 
