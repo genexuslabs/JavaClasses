3e9d9dc20bb0127161d65e8408db771a78d17d04
iroqueta <iroqueta@genexus.com>
2021-11-12 13:03:24 -0300

    Cherry pick branch 'genexuslabs:WebSessionRemove' into beta

--- wrappercommon/src/main/java/com/genexus/websocket/ISession.java
+++ wrappercommon/src/main/java/com/genexus/websocket/ISession.java
@@ -3,6 +3,7 @@ package com.genexus.websocket;
 import java.io.IOException;
 
 public interface ISession {
+	Integer getHashCode();
 	String getQueryString();
 	boolean isOpen();
 	void sendEndPointText(String message) throws IOException;
