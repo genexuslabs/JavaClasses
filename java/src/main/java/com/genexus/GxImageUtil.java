b5c6bd0486dff96c70624241cb85ad358361c8b7
Gonzalo Gallotti <gonzalogallotti@gmail.com>
2022-02-22 11:57:25 -0300

    Cherry pick branch 'genexuslabs:image-api-support-storage' into beta

--- java/src/main/java/com/genexus/GxImageUtil.java
+++ java/src/main/java/com/genexus/GxImageUtil.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import com.genexus.util.GXFile;
 import org.apache.logging.log4j.Logger;
@@ -23,27 +24,28 @@ public class GxImageUtil {
 		return imageFile.startsWith(defaultPath)? imageFile : defaultPath + imageFile.replace("/", File.separator);
 	}
 
+	private static InputStream getInputStream(String imageFile) throws IOException {
+		return new GXFile(imageFile).getStream();
+	}
 	public static long getFileSize(String imageFile){
 		return new File(getImageAbsolutePath(imageFile)).length();
 	}
 
 	public static int getImageHeight(String imageFile) {
-		try {
-			BufferedImage image = ImageIO.read(new File(getImageAbsolutePath(imageFile)));
-			return image.getHeight();
+		try (InputStream is = getInputStream(imageFile)) {
+			return ImageIO.read(is).getHeight();
 		}
-		catch (IOException e) {
+		catch (Exception e) {
 			log.error("getImageHeight " + imageFile + " failed" , e);
 			return 0;
 		}
 	}
 
 	public static int getImageWidth(String imageFile) {
-		try {
-			BufferedImage image = ImageIO.read(new File(getImageAbsolutePath(imageFile)));
-			return image.getWidth();
+		try (InputStream is = getInputStream(imageFile)) {
+			return ImageIO.read(is).getWidth();
 		}
-		catch (IOException e) {
+		catch (Exception e) {
 			log.error("getImageWidth " + imageFile + " failed" , e);
 			return 0;
 		}
