49f964ae1c76a4c3c2bdbcdf285c443a77a2abe9
dalvarellos@genexus.com <dalvarellos@genexus.com>
2022-04-03 21:24:35 -0300

    Refactoring EODynamicCall

--- gxdynamiccall/src/main/java/com/genexus/gxdynamiccall/GXDynamicCall.java
+++ gxdynamiccall/src/main/java/com/genexus/gxdynamiccall/GXDynamicCall.java
@@ -1,7 +1,6 @@
 package com.genexus.gxdynamiccall;
 
 import java.lang.reflect.Array;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import com.genexus.CommonUtil;
@@ -9,156 +8,88 @@ import com.genexus.GXBaseCollection;
 import com.genexus.GXSimpleCollection;
 import com.genexus.SdtMessages_Message;
 import com.genexus.common.interfaces.SpecificImplementation;
-import com.genexus.xml.GXXMLSerializable;
 
 public class GXDynamicCall {
 
-	private String defaultMethod;
-	private String namespace;
 	private String externalName;
 	private String packageName;
-	private GXXMLSerializable properties;
-	private Object object;
-	public String ObjectName;
+	private GXDynCallProperties properties;
+	private Object instanceObject;
+	private String objectName;
 
-	public GXDynamicCall() {
-
-		namespace = null;
-		properties = null;
-		externalName = null;
-		object = null;
-		defaultMethod = "execute";
-	}
-
-	public Object getProperties() {
+	
+	public GXDynCallProperties getProperties() {
 		return properties;
 	}
 
-	public void setProperties(GXXMLSerializable props) {
-		try {
-			properties = props;
-			Field f;
-			f = props.getClass().getDeclaredField("gxTv_SdtJavaProperties_Externalname");
-			f.setAccessible(true);
-			externalName = (String) f.get(props);
-			f = props.getClass().getDeclaredField("gxTv_SdtJavaProperties_Namespace");
-			f.setAccessible(true);
-			namespace = (String) f.get(props);
-			f = props.getClass().getDeclaredField("gxTv_SdtJavaProperties_Packagename");
-			f.setAccessible(true);
-			packageName = (String) f.get(props);
-		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
+	public void setProperties(GXDynCallProperties properties) {
+		this.properties = properties;
+		packageName = properties.getPackageName()==null?SpecificImplementation.Application.getPACKAGE():packageName;
+		externalName = properties.getExternalName();
 	}
 
 	public String getObjectName(){
-		return ObjectName;
+		return objectName;
 	}
 
 	public void setObjectName(String name){
-		this.ObjectName=name;
+		objectName=name;
 	}
 
-	private void VerifyDefaultProperties() {
-		if (packageName.isEmpty()) {
-			packageName = SpecificImplementation.Application.getPACKAGE();
-			if (!packageName.equals("")) {
-				packageName += ".";
-			}
-		}
-
+	public void execute(Object[] parametersArray, Object[] errorsArray) {
+		//Create the instance with default constructor
+		create(null, errorsArray);
+		//Create methodconfiguration
+		GXDynCallMethodConf method = new GXDynCallMethodConf();
+		//Execute with thefault method configuration  
+		execute(parametersArray, method, errorsArray);
 	}
 
-	public void Execute(Object[] parametersArray, Object[] errorsArray) {
+	public Object execute(Object[] parametersArray, GXDynCallMethodConf methodConfiguration, Object[] errorsArray) {
+		
+		GXBaseCollection<SdtMessages_Message> errors =new GXBaseCollection<SdtMessages_Message>();
 		// Take the collection of parameters from de array
-	    GXSimpleCollection<Object> parameters = (GXSimpleCollection<Object>) parametersArray[0]; 
-
-		Create(null, errorsArray);
-		// Take  the collection of Message from  de array
-		GXBaseCollection<SdtMessages_Message> errors = (GXBaseCollection<SdtMessages_Message>) errorsArray[0]; 
-		if (errors.size() == 0) {
-			try {
-				this.ExecuteMethod(this.object, this.defaultMethod, parameters, errors,false);
-				parametersArray[0] = parameters;
-			} catch (Exception e) {
-				CommonUtil.ErrorToMessages("CallMethod Error", e.getMessage(), errors);
-			}
-		}
-	}
-
-	public Object Execute(Object[] parametersArray, GXXMLSerializable methodconfiguration, Object[] errorsArray) {
-		// Take  the collection of Message from  de array
-		GXBaseCollection<SdtMessages_Message> errors = (GXBaseCollection<SdtMessages_Message>) errorsArray[0]; 
-		// Take the collection of parameters from de array
+		GXSimpleCollection<Object> parameters = (GXSimpleCollection<Object>) parametersArray[0]; 
 		Object result=null;
 		Object objectToInvoke;
-	    GXSimpleCollection<Object> parameters = (GXSimpleCollection<Object>) parametersArray[0]; 
-		Field f;
-		boolean isStatic=false;
-		String methodName;
-		try {
-			f = methodconfiguration.getClass().getDeclaredField("gxTv_Java");
-			f.setAccessible(true);
-			GXXMLSerializable methodPlatformSubLevel=(GXXMLSerializable) f.get(methodconfiguration);
-			f = methodPlatformSubLevel.getClass().getDeclaredField("gxTpr_Methodname");
-			f.setAccessible(true);
-			methodName = (String)f.get(methodconfiguration);
-			f = methodPlatformSubLevel.getClass().getDeclaredField("gxTpr_Methodisstatic");
-			f.setAccessible(true);
-			isStatic = (boolean)f.get(methodconfiguration);
-			if (!isStatic)
+		if (!methodConfiguration.getIsStatic())
+		{
+			if (instanceObject != null)
 			{
-				if (this.object != null)
-				{
-					objectToInvoke = this.object;
-					//result = this.ExecuteMethod(this.object, methodName.isEmpty() ? defaultMethod : methodName, parameters, errors, isStatic);
-				}
-				else
-				{
-					objectToInvoke=null;
-					CommonUtil.ErrorToMessages("NullInstance Error", "You must invoke create method before execute a non static one", errors);
-				}
+				objectToInvoke = instanceObject;
 			}
 			else
 			{
-				VerifyDefaultProperties();
-				Class<?> auxClass=null;
-				try {
-					auxClass = loadClass(GXDynamicCall.class.getClassLoader(), this.externalName, packageName);
-				} catch (ClassNotFoundException e) {
-					CommonUtil.ErrorToMessages("Load class Error", e.getMessage(), errors);
-				}
-				objectToInvoke=auxClass;
+				objectToInvoke=null;
+				CommonUtil.ErrorToMessages("NullInstance Error", "You must invoke create method before execute a non static one", errors);
+				return null;
 			}
-			if(errors.getItemCount()==0 && objectToInvoke!=null){
-				result = this.ExecuteMethod(objectToInvoke, methodName.isEmpty() ? defaultMethod : methodName, parameters, errors, isStatic);
-			}
-			errorsArray[0]=errors;
-			parametersArray[0] = parameters;
-		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			CommonUtil.ErrorToMessages("MethodProperties Error", e.getMessage(), errors);
 		}
+		else
+		{
+			Class<?> auxClass=null;
+			try {
+				auxClass = loadClass(externalName, packageName);
+			} catch (ClassNotFoundException e) {
+				CommonUtil.ErrorToMessages("Load class Error", e.getMessage(), errors);
+				errorsArray[0]=errors;
+				return null;
+			}
+			objectToInvoke=auxClass;
+		}
+		result = executeMethod(objectToInvoke,methodConfiguration.getMethodName(), parameters, errors, methodConfiguration.getIsStatic());
+		errorsArray[0]=errors;
+		parametersArray[0] = parameters;
 		return result;
 	}
 
-	public void Create(GXSimpleCollection<Object> constructParameters, Object[] errors) {
-		if (errors == null) {
-			errors = (Object[]) Array.newInstance(Object.class, 1); 
-			errors[0]=new GXBaseCollection<SdtMessages_Message>();
-		}
+	public void create(GXSimpleCollection<Object> constructParameters, Object[] errors) {
+		GXBaseCollection<SdtMessages_Message> error =new GXBaseCollection<SdtMessages_Message>();
 		String objectNameToInvoke;
-
-
-		VerifyDefaultProperties();
-		if (constructParameters == null) {
-			objectNameToInvoke = this.ObjectName;
-		} else {
-			objectNameToInvoke = this.externalName;
-		}
+		objectNameToInvoke = constructParameters==null?objectName:externalName;
 		if (!objectNameToInvoke.isEmpty()) {
 			try {
-				Class<?> objClass = loadClass(GXDynamicCall.class.getClassLoader(), objectNameToInvoke, packageName);
+				Class<?> objClass = loadClass(objectNameToInvoke, packageName);
 				Object[] auxConstParameters;
 				Class<?>[] auxConstructorTypes;
 				if (constructParameters != null && constructParameters.size() > 0) {
@@ -174,23 +105,24 @@ public class GXDynamicCall {
 					auxConstructorTypes = new Class[] { int.class,
 							SpecificImplementation.Application.getModelContextClass() };
 				}
-				object = objClass.getConstructor(auxConstructorTypes).newInstance(auxConstParameters);
+				instanceObject = objClass.getConstructor(auxConstructorTypes).newInstance(auxConstParameters);
 			} catch (Exception e) {
-				CommonUtil.ErrorToMessages("CreateInstance Error", e.getMessage(), (GXBaseCollection<SdtMessages_Message>) errors[0]);
+				CommonUtil.ErrorToMessages("CreateInstance Error", e.getMessage(), (GXBaseCollection<SdtMessages_Message>) error);
 			}
 		}
 		else{
-			CommonUtil.ErrorToMessages("CreateInstance Error", "Object name not set", (GXBaseCollection<SdtMessages_Message>) errors[0]);
+			CommonUtil.ErrorToMessages("CreateInstance Error", "Object name not set", (GXBaseCollection<SdtMessages_Message>) error);
 		}
+		errors[0]=error;
 	}
 
-	private Object ExecuteMethod(Object objectToInvoke, String method, GXSimpleCollection<Object> params, GXBaseCollection<SdtMessages_Message> errors, boolean isStatic) {
-		// Get Method
+	private Object executeMethod(Object objectToInvoke, String method, GXSimpleCollection<Object> params, GXBaseCollection<SdtMessages_Message> errors, boolean isStatic) {
+		
 		Object returnObject = null;
 		Method[] methods;
 		Method methodToExecute = null;
-		Class<?>[] paramsType=new Class<?>[params.getItemCount()];
-		Object[] callingParams = new Object[params.getItemCount()]; // Array to pass to method.invoke
+		Class<?>[] paramsType=new Class<?>[params.size()];
+		Object[] callingParams = new Object[params.size()]; // Array to pass to method.invoke
 		try{
 			for (int i=0; i < params.size(); i++) {
 				paramsType[0]=params.get(i).getClass();	
@@ -231,6 +163,7 @@ public class GXDynamicCall {
 									array[0] = CommonUtil.convertObjectTo(parm, destClass);
 								} else {
 									CommonUtil.ErrorToMessages("CallMethod Error", "IllegalArgumentException - Type does not match", errors);
+									return null;
 								}
 							} else {
 								array[0] = parm;
@@ -243,6 +176,7 @@ public class GXDynamicCall {
 									callingParams[i] = CommonUtil.convertObjectTo(parm, destClass);
 								} else {
 									CommonUtil.ErrorToMessages("CallMethod Error", "IllegalArgumentException - Type does not match", errors);
+									return null;
 								}
 							} else {
 								callingParams[i] = parm;
@@ -251,24 +185,25 @@ public class GXDynamicCall {
 						i++;
 					} catch (Exception ex) {
 						CommonUtil.ErrorToMessages("CallMethod Error", ex.getMessage(), errors);
+						return null;
 					}
 				}
 			}
 			else{
 				CommonUtil.ErrorToMessages("CallMethod Error","NoSuchMethodException - Cant finde method: " + method + "with " + params.size() + " parameters", errors);
+				return null;
 			}
 		}
-		if (methodToExecute != null) {
-			try {
-				// Execute the method
-				returnObject = methodToExecute.invoke(isStatic?null:objectToInvoke, callingParams);
-				updateParams(params, callingParams);
-			} catch (Exception e) {
-				CommonUtil.ErrorToMessages("CallMethod Error", e.getMessage(), errors);
-			}
-		} else {
-			CommonUtil.ErrorToMessages("CallMethod Error","NoSuchMethodException - Cant finde method: " + method + "with " + params.size() + " parameters", errors);
+		
+		try {
+			// Execute the method
+			returnObject = methodToExecute.invoke(isStatic?null:objectToInvoke, callingParams);
+			updateParams(params, callingParams);
+		} catch (Exception e) {
+			CommonUtil.ErrorToMessages("CallMethod Error", e.getMessage(), errors);
+			return null;
 		}
+		
 		return returnObject;
 	}
 
@@ -296,30 +231,11 @@ public class GXDynamicCall {
 		}
 	}
 
-	private Class<?> loadClass(ClassLoader cLoader, String className, String sPackage) throws ClassNotFoundException {
-		String classPackage = SpecificImplementation.Application.getPACKAGE();
-		if (!classPackage.equals(""))
-			classPackage += ".";
-		String pgmName = CommonUtil.getObjectName(sPackage, className);
-		Class<?> c = null;
-		if (!classPackage.equals(sPackage)) {
-
-			try {
-				c = Class.forName(pgmName);
-			} catch (ClassNotFoundException e) {
-				pgmName = CommonUtil.getObjectName(classPackage, className);
-			}
-		}
-
-		if (c == null) {
-			if (cLoader == null) {
-				c = Class.forName(pgmName);
-			} else {
-
-				c = cLoader.loadClass(pgmName);
-			}
-		}
+	private Class<?> loadClass(String className, String sPackage) throws ClassNotFoundException {
+		String classPackage = sPackage + "." + className;
+		Class<?> c = Class.forName(classPackage);;
 		return c;
 	}
-
+//usar .size
+//returns y no preguntar por los null
 }
