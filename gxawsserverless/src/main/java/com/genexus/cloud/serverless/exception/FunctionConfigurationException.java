31c3b92be2dd353a6ed6ba1a29a9182a9437d02a
Gonzalo Gallotti <gonzalogallotti@gmail.com>
2022-05-19 09:55:58 -0300

    Code refactoring

---
+++ gxawsserverless/src/main/java/com/genexus/cloud/serverless/exception/FunctionConfigurationException.java
@@ -0,0 +1,7 @@
+package com.genexus.cloud.serverless.exception;
+
+public class FunctionConfigurationException extends Exception {
+	public FunctionConfigurationException(String exceptionMessage) {
+		super(exceptionMessage);
+	}
+}
