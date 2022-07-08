b7adcc9053b60fa3034705aada0b92616d5dc67e
Gonzalo Gallotti <gonzalogallotti@gmail.com>
2022-07-08 10:44:52 -0300

    Cherry pick branch 'genexuslabs:cors-filter' into beta

--- wrapperjakarta/src/main/java/com/genexus/servlet/CorsFilter.java
+++ wrapperjakarta/src/main/java/com/genexus/servlet/CorsFilter.java
@@ -18,7 +18,7 @@ public class CorsFilter implements jakarta.servlet.Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		Filter.super.init(filterConfig);
+
 	}
 
 	@Override
@@ -41,6 +41,6 @@ public class CorsFilter implements jakarta.servlet.Filter {
 
 	@Override
 	public void destroy() {
-		Filter.super.destroy();
+
 	}
 }
