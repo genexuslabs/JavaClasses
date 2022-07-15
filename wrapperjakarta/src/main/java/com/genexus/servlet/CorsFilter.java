a9d788372672116ac8087d0ec0c91ce0d69ff3ef
Gonzalo <gonzalogallotti@gmail.com>
2022-07-15 16:30:16 -0300

    Add CORS Headers to JAX Webservice Responses when Environment Variable is Present (#585)
    * Add CORS Filter
    
    * Add CorsFilter for old plain webservices that are served through servlet and not JAXRS.
    
    * Adjust Http Headers for CORS
    
    * Do not call superclass

---
+++ wrapperjakarta/src/main/java/com/genexus/servlet/CorsFilter.java
@@ -0,0 +1,46 @@
+package com.genexus.servlet;
+
+
+import com.genexus.cors.CORSHelper;
+import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class CorsFilter implements jakarta.servlet.Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+		HashMap<String, String> corsHeaders = CORSHelper.getCORSHeaders(request.getHeader("Access-Control-Request-Headers"));
+		if (corsHeaders != null) {
+			HttpServletResponse response = (HttpServletResponse) servletResponse;
+			for (String headerName : corsHeaders.keySet()) {
+				if (!response.containsHeader(headerName)) {
+					response.setHeader(headerName, corsHeaders.get(headerName));
+				}
+			}
+		}
+		if (!request.getMethod().equalsIgnoreCase("OPTIONS")) {
+			filterChain.doFilter(servletRequest, servletResponse);
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
