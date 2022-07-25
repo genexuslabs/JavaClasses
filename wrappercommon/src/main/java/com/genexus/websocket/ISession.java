1f46b8879fdfbbaf7904eb9dafed6b4fba658b6c
Gonzalo <gonzalogallotti@gmail.com>
2022-07-25 09:58:45 -0300

    WebSocket & WebNotifications: Stability and Logging Improvements (#591)
    * Improvements to WebSocket implementation
    
    * Add logging
    
    * Resolve @iroqueta review
    
    * Improve messages
    
    * @iroqueta review
    
    * Refactoring and improvements
    
    * Removed unused services variable
    
    * Revet logging
    
    * Refactoring

--- wrappercommon/src/main/java/com/genexus/websocket/ISession.java
+++ wrappercommon/src/main/java/com/genexus/websocket/ISession.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public interface ISession {
 	Integer getHashCode();
-	String getQueryString();
+	String getId();
 	boolean isOpen();
 	void sendEndPointText(String message) throws IOException;
 }
