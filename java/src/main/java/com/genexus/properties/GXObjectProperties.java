e3b2f742e7a72d60af5860409bc8fbebed9620e7
AlejandroP <apanizza@genexus.com>
2022-08-17 20:19:22 -0300

    - Add remote object property management for APIObject calls

---
+++ java/src/main/java/com/genexus/properties/GXObjectProperties.java
@@ -0,0 +1,16 @@
+package com.genexus.properties;
+
+import com.genexus.internet.*;
+
+public class GXObjectProperties
+{
+	private Location location = null;
+	public Location getLocation()
+	{
+		return location; 
+	}
+	public void setLocation(Location value)
+	{
+		location = value;
+	}
+}
