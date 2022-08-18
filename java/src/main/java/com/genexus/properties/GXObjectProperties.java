68b6912bf8c17c18e7d366d472873ca95ebb2b9c
AlejandroP <apanizza@genexus.com>
2022-08-18 04:01:44 -0300

    - Added properties to handle errors.

--- java/src/main/java/com/genexus/properties/GXObjectProperties.java
+++ java/src/main/java/com/genexus/properties/GXObjectProperties.java
@@ -5,6 +5,10 @@ import com.genexus.internet.*;
 public class GXObjectProperties
 {
 	private Location location = null;
+	private String errorMessage = "";
+	private int errorCode = 0;
+	private int statusCode = 0;
+	
 	public Location getLocation()
 	{
 		return location; 
@@ -12,5 +16,32 @@ public class GXObjectProperties
 	public void setLocation(Location value)
 	{
 		location = value;
+	}
+
+	public int getErrorCode()
+	{
+		return errorCode; 
+	}
+	public void setErrorCode(int value)
+	{
+		errorCode = value;
+	}
+
+	public int getStatusCode()
+	{
+		return statusCode; 
+	}
+	public void setStatusCode(int value)
+	{
+		statusCode = value;
+	}
+
+	public String getErrorMessage()
+	{
+		return errorMessage; 
+	}
+	public void setErrorMessage(String value)
+	{
+		errorMessage = value;
 	}
 }
