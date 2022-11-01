b6858d88050b393bc5021c4eced78fd62c3f1bbd
Alejandro Panizza Carve <apanizza@genexus.com>
2022-11-01 01:07:56 -0300

    - Add Rest API Client implementation. (#597)
    * - Add Rest API Client implementation.
    
    * - Eliminate debug/trace messages.
    
    * - Add remote object property management for APIObject calls
    
    * - Added properties to handle errors.
    
    * - Add method for file upload in Rest API Client
    
    * - Fix for datetime format with milliseconds , the pattern was wrong
    
    * -Added methods for BC objects and Decimal values in GXRestAPI
    
    * - Fixed type error in GetBodySdt methods names
    
    * - Added Body Method for special objects in GXRestAPIClient
    
    * - Fix format for datetime with milliseconds
    
    * - Add geography type routines for RestAPI calls
    
    * - Fix for SDT o BC input vars in REST GET (APi Object)
    
    * - Escape String and geography types in GET and DELETE Rest Services (API object)
    
    * - Remove IGeoJson interface and unify object parse routines.
    - Add constants and log for error management
    - Fix code style
    - Move GXObjectProperties implementation to gxcommon
    
    * - Fix code imports.

---
+++ common/src/main/java/com/genexus/properties/GXObjectsConfiguration.java
@@ -0,0 +1,15 @@
+package  com.genexus.properties;
+
+import java.util.HashMap;
+
+public class GXObjectsConfiguration {
+		
+	private  HashMap<String, GXObjectProperties> properties = new HashMap<String, GXObjectProperties>();	
+
+	public GXObjectProperties propertiesFor(String objName)
+	{
+		if (!properties.containsKey(objName))
+			properties.put(objName, new GXObjectProperties());
+		return properties.get(objName);
+	}
+}
