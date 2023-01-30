43d4949e599ea7904d5b67344ebd51dbda7827cf
iroqueta <iroqueta@genexus.com>
2023-01-30 16:21:03 -0300

    Remove gxandroidpublisher module Issue: 101136

--- gxandroidpublisher/src/main/java/com/genexus/sd/store/validation/model/ProductType.java
+++
@@ -1,6 +0,0 @@
-package com.genexus.sd.store.validation.model;
-
-public class ProductType {
-	public static final int Product = 1;
-	public static final int Subscription = 2;
-}
--- gxandroidpublisher/src/main/java/com/genexus/sd/store/validation/model/exceptions/StorePurchaseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.genexus.sd.store.validation.model.exceptions;
-
-public class StorePurchaseNotFoundException extends Exception {
-	public StorePurchaseNotFoundException(String errMessage){
-		super(errMessage);
-	}
-}
