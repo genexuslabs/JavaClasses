6acbb6be73554e06dbc2c6ed215c39ba28ccef6b
tomas-sexenian <tsexenian@genexus.com>
2023-01-31 12:32:49 -0300

    Revert "convertObjectTo returns null Str if obj is "null""
    This reverts commit 7a55fff4925b8282f2d0e7c81e6303ad30ffaf1e.

--- common/src/main/java/com/genexus/CommonUtil.java
+++ common/src/main/java/com/genexus/CommonUtil.java
@@ -2725,9 +2725,10 @@ public final class CommonUtil
 		{
 			try
             {
-				if (objStr.isEmpty() || objStr.equals("null"))
+            	
+				if (objStr.isEmpty())
 					objStr ="0";
-				else
+				else 
 				{
 					int i = objStr.indexOf(".") ;
 					if	(i >= 0)
@@ -2747,7 +2748,7 @@ public final class CommonUtil
             }
 			if (className.equals("short") || className.equals("java.lang.Short") || className.equals("[S"))		   
     	    {
-        	    return Short.valueOf(objStr);
+        	    return Short.valueOf(objStr);        
         	}
         	else if (className.equals("byte") || className.equals("java.lang.Byte") || className.equals("[B"))
         	{
@@ -2764,7 +2765,7 @@ public final class CommonUtil
         }
         else if (className.equals("string") || className.indexOf("java.lang.String") != -1)
         {
-            return objStr.equals("null") ? null : objStr;
+            return objStr;
         }
         else if (className.equals("double") || className.equals("java.lang.Double") || className.equals("[D"))
         {
