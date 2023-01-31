10ecf20fb2f90a6333a84c21219dfe913ab9b15d
iroqueta <iroqueta@genexus.com>
2023-01-31 09:24:05 -0300

    Create a new module called gxweb containing classes from Frontend. Previously this clases were in gxclassR module. Move old Crypto data type classes to gxcryptocommon module and remove reference to gxcryptocommon from gxclassR. Issue: 101136

--- java/src/main/java/com/genexus/webpanels/GXWebObjectBase.java
+++ java/src/main/java/com/genexus/webpanels/GXWebObjectBase.java
@@ -672,4 +672,10 @@ public abstract class GXWebObjectBase extends GXRestServiceWrapper implements IE
 		if(dbgInfo != null)
 			dbgInfo.trkRng(lineNro, colNro, lineNro2, colNro2);
 	}
+
+	protected void callWebObject(String url)
+	{
+		httpContext.wjLoc = url;
+	}
+
 }
