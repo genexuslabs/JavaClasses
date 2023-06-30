91a34742fc924543c5a5765369f185b0f4a39a46
tomas-sexenian <tsexenian@genexus.com>
2023-06-30 10:45:39 -0300

    Third wave of security fixes.

--- common/src/main/java/com/genexus/xml/XMLReader.java
+++ common/src/main/java/com/genexus/xml/XMLReader.java
@@ -851,12 +851,12 @@ public class XMLReader implements XMLDocumentHandler, XMLErrorHandler, XMLDTDHan
 	public void openRequest(IHttpRequest client)
 	{
 		reset();
-		try
+		try (InputStream is = client.getInputStream())
 		{
 			if (documentEncoding.length() > 0)
-				inputSource = new XMLInputSource(null, null, null, client.getInputStream(), documentEncoding);
+				inputSource = new XMLInputSource(null, null, null, is, documentEncoding);
 			else
-				inputSource = new XMLInputSource(null, null, null, client.getInputStream(), null);
+				inputSource = new XMLInputSource(null, null, null, is, null);
 			parserConfiguration.setInputSource(inputSource);
 		}
 		catch (IOException e)
