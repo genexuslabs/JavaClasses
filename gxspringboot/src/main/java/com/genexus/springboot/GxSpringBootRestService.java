dc8a13de28a5eef47c3c9722eb0c97590683d311
iroqueta <iroqueta@genexus.com>
2023-08-16 12:57:12 -0300

    Error code in response error message must be numeric in springboot Issue: 103655

--- gxspringboot/src/main/java/com/genexus/springboot/GxSpringBootRestService.java
+++ gxspringboot/src/main/java/com/genexus/springboot/GxSpringBootRestService.java
@@ -1,7 +1,8 @@
 package com.genexus.springboot;
 
 import com.genexus.GxRestService;
-import com.genexus.WrapperUtils;
+import json.org.json.JSONException;
+import json.org.json.JSONObject;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +14,19 @@ abstract public class GxSpringBootRestService extends GxRestService {
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	protected ResponseEntity<Object> handleException(HttpMessageNotReadableException ex) {
 		log.error("Error executing REST service", ex);
+		JSONObject errorJson = new JSONObject();
+		try
+		{
+			JSONObject obj = new JSONObject();
+			obj.put("code", 400);
+			obj.put("message", "Bad Request");
+			errorJson.put("error", obj);
+		}
+		catch(JSONException e)
+		{
+			log.error("Invalid JSON", e);
+		}
 
-		SetError("400", "Bad Request");
 		ResponseEntity.BodyBuilder builder = ResponseEntity.status(HttpStatus.BAD_REQUEST);
 		builder.contentType(MediaType.APPLICATION_JSON);
 		return builder.body(errorJson.toString()) ;
