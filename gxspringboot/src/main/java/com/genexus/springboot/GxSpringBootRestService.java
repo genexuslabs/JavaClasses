fd2d5e19c8954e82da9f2220928dd7a151007e97
iroqueta <iroqueta@genexus.com>
2023-08-29 14:47:46 -0300

    Catch general error to wirte a response with Internal Server Error in REST services using Spring Boot Image.Link() in Spring Boot cames without contextPath Issue: 103655

--- gxspringboot/src/main/java/com/genexus/springboot/GxSpringBootRestService.java
+++ gxspringboot/src/main/java/com/genexus/springboot/GxSpringBootRestService.java
@@ -11,15 +11,25 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 abstract public class GxSpringBootRestService extends GxRestService {
 	private static Logger log = org.apache.logging.log4j.LogManager.getLogger(GxSpringBootRestService.class);
 
-	@ExceptionHandler(value = {HttpMessageNotReadableException.class, NullPointerException.class})
+	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<Object> handleException(Exception ex) {
 		log.error("Error executing REST service", ex);
 		JSONObject errorJson = new JSONObject();
+		int errCode = 500;
+		String errMessage = "Internal Server Error";
+		HttpStatus statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+
+		if (ex instanceof HttpMessageNotReadableException || ex instanceof NullPointerException) {
+			errCode = 400;
+			errMessage = "Bad Request";
+			statusCode = HttpStatus.BAD_REQUEST;
+		}
+
 		try
 		{
 			JSONObject obj = new JSONObject();
-			obj.put("code", 400);
-			obj.put("message", "Bad Request");
+			obj.put("code", errCode);
+			obj.put("message", errMessage);
 			errorJson.put("error", obj);
 		}
 		catch(JSONException e)
@@ -27,7 +37,7 @@ abstract public class GxSpringBootRestService extends GxRestService {
 			log.error("Invalid JSON", e);
 		}
 
-		ResponseEntity.BodyBuilder builder = ResponseEntity.status(HttpStatus.BAD_REQUEST);
+		ResponseEntity.BodyBuilder builder = ResponseEntity.status(statusCode);
 		builder.contentType(MediaType.APPLICATION_JSON);
 		return builder.body(errorJson.toString()) ;
 	}
