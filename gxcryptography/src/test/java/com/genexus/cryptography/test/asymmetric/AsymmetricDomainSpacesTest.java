e90e8e5c68eb8b3bfd4f0c297d4cf11fef3346cf
ARTECH\sgrampone <sgrampone@genexus.com>
2024-05-20 16:34:52 -0300

    Cherry pick branch 'genexuslabs:securityapicommons' into beta

---
+++ gxcryptography/src/test/java/com/genexus/cryptography/test/asymmetric/AsymmetricDomainSpacesTest.java
@@ -0,0 +1,57 @@
+package com.genexus.cryptography.test.asymmetric;
+
+import com.genexus.cryptography.asymmetric.AsymmetricCipher;
+import com.genexus.securityapicommons.keys.CertificateX509;
+import com.genexus.securityapicommons.keys.PrivateKeyManager;
+import com.genexus.securityapicommons.utils.SecurityUtils;
+import com.genexus.test.commons.SecurityAPITestObject;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AsymmetricDomainSpacesTest extends SecurityAPITestObject{
+
+	private static String path_RSA_sha1_1024;
+	private static String plainText;
+	private static PrivateKeyManager key;
+	private static CertificateX509 cert;
+	private static String pathKey;
+	private static String pathCert;
+	private static AsymmetricCipher asymCipher;
+
+	@Override
+	protected void setUp() {
+		path_RSA_sha1_1024 = "C:\\Temp\\dummycerts\\RSA_sha1_1024\\";
+		plainText = "Lorem ipsum";
+		pathKey = path_RSA_sha1_1024 + "sha1d_key.pem";
+		pathCert = path_RSA_sha1_1024 + "sha1_cert.crt";
+		key = new PrivateKeyManager();
+		cert = new CertificateX509();
+		asymCipher = new AsymmetricCipher();
+
+	}
+
+	public static Test suite() {
+		return new TestSuite(AsymmetricDomainSpacesTest.class);
+	}
+
+	@Override
+	public void runTest() {
+		testSpaces();
+	}
+
+	public void testSpaces()
+	{
+
+		key.load(pathKey);
+		cert.load(pathCert);
+		String encrypted1 = asymCipher.doEncrypt_WithPrivateKey("SHA1 ", "PCKS1PADDING ", key, plainText);
+		assertFalse(asymCipher.hasError());
+
+		String decrypted = asymCipher.doDecrypt_WithPublicKey(" SHA1", " PCKS1PADDING", cert, encrypted1);
+		assertFalse(asymCipher.hasError());
+		assertTrue(SecurityUtils.compareStrings(plainText, decrypted));
+	}
+
+
+}
