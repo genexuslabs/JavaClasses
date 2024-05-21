f02b8e0232735d3cc5f9d90618d4beda3a664161
ARTECH\sgrampone <sgrampone@genexus.com>
2024-05-21 17:02:50 -0300

    Cherry pick branch 'genexuslabs:securityapicommons' into beta

--- gxcryptography/src/test/java/com/genexus/cryptography/test/checksum/ChecksumTest.java
+++ gxcryptography/src/test/java/com/genexus/cryptography/test/checksum/ChecksumTest.java
@@ -13,12 +13,9 @@ public class ChecksumTest extends SecurityAPITestObject {
 	private static String input;
 	private static String b64Input;
 	private static String hexaInput;
-	private static String localFileInput;
 	private static String[] algorithms;
 	private static String[] results;
 	private static String[] results0x;
-	private static String[] resultsLocalFile;
-	private static String[] resultsLocalFile0x;
 	private static String[] inputTypes;
 	private static String[] inputs;
 
@@ -28,7 +25,6 @@ public class ChecksumTest extends SecurityAPITestObject {
 		input = "123456789";
 		b64Input = "MTIzNDU2Nzg5";
 		hexaInput = "313233343536373839";
-		localFileInput = resources.concat("/bookSample.xml");
 
 		inputs = new String[] { input, input, b64Input, hexaInput };
 		inputTypes = new String[] { "ASCII", "TXT", "BASE64", "HEX" };
@@ -55,21 +51,6 @@ public class ChecksumTest extends SecurityAPITestObject {
 			"0xF7C3BC1D808E04732ADF679965CCC34CA7AE3441",
 			"0x15E2B0D3C33891EBB0F1EF609EC419420C20E320CE94C65FBC8C3312448EB225",
 			"0xD9E6762DD1C8EAF6D61B3C6192FC408D4D6D5F1176D0C29169BC24E71C3F274AD27FCD5811B313D681F7E55EC02D73D499C95455B6B5BB503ACF574FBA8FFE85" };
-		resultsLocalFile = new String[] { "AD", "C2", "C0", "29", "AC", "C6", "F8", "58", "E9", "BA", "E7BF", "EB02",
-			"7982", "8DC7", "8023", "347E", "78ED", "78EC", "1223", "F11E", "14FD", "867D", "8690", "8656", "89CC",
-			"D6A3", "FF40", "1452", "47D3", "12A7", "EBAD", "796F", "072B", "3172BC69", "18D27AE6", "8C403DED",
-			"3B83323F", "E72D8519", "981F81DA", "323A5936", "CE8D4396", "ABBC544E",
-			"19D987515BC23D6D8A29AD2A063A58ED", "1DCC48655437412E56A0B66E214DE0A91B0F888F",
-			"5248D4A7D1A6588AB8BABDE9487D43579CA957DED6ED553AF04D506702100C9F",
-			"5C7B1671D81B37FB4A1B8DF4E23091D10181F2D3559BD3B5515C23F8F042CF8A60E26EAC5A70D8E2AC7B8585CAAB7F42480213D5771FC5430E550F2D429349FA" };
-		resultsLocalFile0x = new String[] { "0xAD", "0xC2", "0xC0", "0x29", "0xAC", "0xC6", "0xF8", "0x58", "0xE9",
-			"0xBA", "0xE7BF", "0xEB02", "0x7982", "0x8DC7", "0x8023", "0x347E", "0x78ED", "0x78EC", "0x1223",
-			"0xF11E", "0x14FD", "0x867D", "0x8690", "0x8656", "0x89CC", "0xD6A3", "0xFF40", "0x1452", "0x47D3",
-			"0x12A7", "0xEBAD", "0x796F", "0x072B", "0x3172BC69", "0x18D27AE6", "0x8C403DED", "0x3B83323F",
-			"0xE72D8519", "0x981F81DA", "0x323A5936", "0xCE8D4396", "0xABBC544E",
-			"0x19D987515BC23D6D8A29AD2A063A58ED", "0x1DCC48655437412E56A0B66E214DE0A91B0F888F",
-			"0x5248D4A7D1A6588AB8BABDE9487D43579CA957DED6ED553AF04D506702100C9F",
-			"0x5C7B1671D81B37FB4A1B8DF4E23091D10181F2D3559BD3B5515C23F8F042CF8A60E26EAC5A70D8E2AC7B8585CAAB7F42480213D5771FC5430E550F2D429349FA" };
 	}
 
 	public static Test suite() {
@@ -79,9 +60,8 @@ public class ChecksumTest extends SecurityAPITestObject {
 	@Override
 	public void runTest() {
 		testBulkChecksum();
-		//testBulkChecksum_LocalFile();
 		testBulkChecksum0x();
-		//testBulkChecksum0x_LocalFile();*/
+
 	}
 
 	public void testBulkChecksum() {
@@ -109,26 +89,5 @@ public class ChecksumTest extends SecurityAPITestObject {
 		}
 
 	}
-
-	/*public void testBulkChecksum_LocalFile() {
-		ChecksumCreator check = new ChecksumCreator();
-		for (int k = 0; k < algorithms.length; k++) {
-			String checksum = check.generateChecksum(localFileInput, "LOCAL_FILE", algorithms[k]);
-			assertTrue(SecurityUtils.compareStrings(checksum, resultsLocalFile[k]));
-			boolean verify = check.verifyChecksum(localFileInput, "LOCAL_FILE", algorithms[k], checksum);
-			True(verify, check);
-		}
-	}
-
-	public void testBulkChecksum0x_LocalFile() {
-		ChecksumCreator check = new ChecksumCreator();
-		for (int k = 0; k < algorithms.length; k++) {
-			String checksum = check.generateChecksum(localFileInput, "LOCAL_FILE", algorithms[k]);
-			assertTrue(SecurityUtils.compareStrings(checksum, resultsLocalFile[k]));
-			boolean verify = check.verifyChecksum(localFileInput, "LOCAL_FILE", algorithms[k],
-				resultsLocalFile0x[k].toLowerCase());
-			True(verify, check);
-		}
-	}*/
 
 }
