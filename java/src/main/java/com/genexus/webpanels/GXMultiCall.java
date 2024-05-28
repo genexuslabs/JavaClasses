556f283a1c8259748d8a60f68b200d549cf6239a
iroqueta <iroqueta@genexus.com>
2024-05-28 13:03:44 -0300

    Remove org.json lib fork and use new version of the API https://github.com/stleary/JSON-java Issue: 105607 d844074

--- java/src/main/java/com/genexus/webpanels/GXMultiCall.java
+++ java/src/main/java/com/genexus/webpanels/GXMultiCall.java
@@ -85,7 +85,7 @@ public class GXMultiCall extends GxRestService
 			Object [] params = new Object[parmCount];
 			for(int j = 0; j < parmCount; j++)
 			{
-				params[j] = GXutil.convertObjectTo(procParms.getString(j), parameters[j]);
+				params[j] = GXutil.convertObjectTo(procParms.get(j).toString(), parameters[j]);
 			}
 			com.genexus.db.DynamicExecute.dynamicExecute(modelContext, remoteHandle, modelContext.packageClass, appPackage + procName, params);
 		}
