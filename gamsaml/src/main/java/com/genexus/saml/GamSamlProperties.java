f06ccbfd60498e4e1246b527bae831b9b409025a
ARTECH\sgrampone <sgrampone@genexus.com>
2024-05-30 12:58:56 -0300

    SAML GAM EO migration

---
+++ gamsaml/src/main/java/com/genexus/saml/GamSamlProperties.java
@@ -0,0 +1,346 @@
+package com.genexus.saml;
+
+import com.genexus.diagnostics.core.ILogger;
+import com.genexus.diagnostics.core.LogManager;
+
+import java.io.StringReader;
+import java.lang.reflect.Method;
+import java.util.Properties;
+
+
+public class GamSamlProperties {
+	public static final ILogger logger = LogManager.getLogger(GamSamlProperties.class);
+	private static GamSamlProperties instance = null;
+	private Properties generalPropsProperties = null;
+	private Properties attPropsProperties = null;
+	private static String state = "";
+
+	private GamSamlProperties() {
+	}
+
+	public static GamSamlProperties getInstance() {
+		if (instance == null) {
+			instance = new GamSamlProperties();
+		}
+		return instance;
+	}
+
+	public void init(String instate) {
+		logger.debug("[init] - state = " + instate);
+		generalPropsProperties = null;
+		if (instate == null || instate.isEmpty()) {
+			logger.debug("[init] - state state == null or empty");
+			state = "";
+			logger.debug("[init] sale if ");
+
+		} else {
+			state = instate;
+			logger.debug("[init] - state != null");
+			// if state is there inicialize from GAM
+			try {
+				generalPropsProperties = loadPropsFromGAM(instate);
+			} catch (Exception e) {
+				logger.error("[init] ", e);
+			}
+		}
+	}
+
+	public void updatePropsFromGAM(String state) {
+		logger.debug("[init] - updatePropsFromGAM = " + state);
+		generalPropsProperties = null;
+		try {
+			generalPropsProperties = loadPropsFromGAM(state);
+		} catch (Exception e) {
+			logger.error("[] updatePropsFromGAM " , e);
+		}
+	}
+
+	public static String getSingleLogoutendpoint() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"SingleLogoutEndpoint");
+		logger.debug("[getSingleLogoutendpoint] " + "Gets SingleLogoutendpoint: " + ret);
+		return ret;
+	}
+
+	public static String getSingleLogoutLocation() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"SingleLogoutLocation");
+		logger.debug("[getSingleLogoutLocation] " + "Gets SingleLogoutLocation: " + ret);
+		return ret;
+	}
+
+	public static String getSingleLogoutResponseLocation() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"SingleLogoutResponseLocation");
+		logger.debug("[getSingleLogoutResponseLocation] " + "Gets SingleLogoutResponseLocation: " + ret);
+		return ret;
+	}
+
+	public static String getServiceProviderEntityId() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"ServiceProviderEntityId");
+		logger.debug("[getServiceProviderEntityId] " + "Gets ServiceProviderEntityId: " + ret);
+		return ret;
+	}
+
+	public static String getSamlEndpointLocation() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"SamlEndpointLocation");
+		logger.debug("[getSamlEndpointLocation] " + "Gets SamlEndpointLocation: " + ret);
+		return ret;
+	}
+
+	public static String getNameIDPolicyFormat() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"NameIDPolicyFormat");
+		logger.debug("[getNameIDPolicyFormat] " + "Gets NameIDPolicyFormat: " + ret);
+		return ret;
+	}
+
+	public static String getAuthnContext() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"AuthnContext");
+		logger.debug("[getAuthnContext] " + "Gets AuthnContext: " + ret);
+		return ret;
+	}
+
+	public static String getForceAuthn() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"ForceAuthn");
+		logger.debug("[getForceAuthn] " + "Gets ForceAuthn: " + ret);
+		return ret;
+	}
+
+	public static String getKeyStPathCredential() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"KeyStPathCredential");
+		logger.debug("[getKeyStPathCredential] " + "Gets KeyStPathCredential: " + ret);
+		return ret;
+	}
+
+	public static String getKeyStPwdCredential() {
+		String cryptPass = GamSamlProperties.getInstance().getGeneralProperty(
+			"KeyStPwdCredential");
+		String ret = Crypt.getInstance().Decrypt(cryptPass);
+		logger.debug("[getKeyStPwdCredential] " + "Gets getKeyStPwdCredential: " + ret);
+
+		return ret;
+	}
+
+	public static String getKeyAliasCredential() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"KeyAliasCredential");
+		logger.debug("[getKeyAliasCredential] " + "Gets getKeyAliasCredential: " + ret);
+		return ret;
+	}
+
+	public static String getKeyStoreCredential() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"KeyStoreCredential");
+		logger.debug("[getKeyStoreCredential] " + "Gets getKeyStoreCredential: " + ret);
+
+		return ret;
+	}
+
+	public static String getKeyStPathTrustSt() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"KeyStoreFilePathTrustCred");
+		logger.debug("[getKeyStPathTrustSt] " + "Gets KeyStoreFilePathTrustCred: " + ret);
+		return ret;
+	}
+
+	public static String getKeyStPwdTrustSt() {
+		String cryptPass = GamSamlProperties.getInstance().getGeneralProperty(
+			"KeyStorePwdTrustCred");
+		String ret = Crypt.getInstance().Decrypt(cryptPass);
+		logger.debug("[getKeyStPwdTrustSt] " + "Gets KeyStorePwdTrustCred: " + cryptPass);
+		return ret;
+	}
+
+	public static String getKeyStoreTrustSt() {
+		return GamSamlProperties.getInstance()
+			.getGeneralProperty("KeyStoreTrustCred");
+	}
+
+	public static String getKeyAliasTrustSt() {
+		return GamSamlProperties.getInstance().getGeneralProperty("KeyAliasTrustSt");
+	}
+
+	public static String getKeyStoreFilePathTrustCred() {
+		String ret = GamSamlProperties.getInstance().getGeneralProperty(
+			"KeyStoreFilePathTrustCred");
+		logger.debug("[getKeyStoreFilePathTrustCred] " + "Gets KeyStoreFilePathTrustCred: " + ret);
+		return ret;
+	}
+
+	public static String getKeyStorePwdTrustCred() {
+		String cryptPass = GamSamlProperties.getInstance().getGeneralProperty(
+			"KeyStorePwdTrustCred");
+		logger.debug("[getKeyStorePwdTrustCred] " + "cryptPass: " + cryptPass);
+		String ret = Crypt.getInstance().Decrypt(cryptPass);
+		logger.debug("[getKeyStorePwdTrustCred] " + "Gets KeyStorePwdTrustCred: " + ret);
+		return ret;
+	}
+
+	public static String getKeyCrypt() {
+		return GamSamlProperties.getInstance().getGeneralProperty("KeyCrypt");
+	}
+
+	public static String getKeyAliasTrustCred() {
+		return GamSamlProperties.getInstance()
+			.getGeneralProperty("KeyAliasTrustCred");
+	}
+
+	public static String getKeyStoreTrustCred() {
+		return GamSamlProperties.getInstance()
+			.getGeneralProperty("KeyStoreTrustCred");
+	}
+
+	public static String getPathErrorServletSSO() {
+		return GamSamlProperties.getInstance().getGeneralProperty(
+			"PathErrorServletSSO");
+	}
+
+	public static String getPathLoginServletSSO() {
+		return GamSamlProperties.getInstance().getGeneralProperty(
+			"PathLoginServletSSO");
+	}
+
+	public static String getPathCancelServletSSO() {
+		return GamSamlProperties.getInstance().getGeneralProperty(
+			"PathCancelServletSSO");
+	}
+
+	public static String getCriteriaKey() {
+		String cryptPass = GamSamlProperties.getInstance().getGeneralProperty("CriteriaKey");
+
+		String ret = Crypt.getInstance().Decrypt(cryptPass);
+		logger.debug("[getCriteriaKey] " + "Gets getCriteriaKey: " + ret);
+
+		return ret;
+	}
+
+	public static String isSmartCardContext() {
+		return GamSamlProperties.getInstance()
+			.getGeneralProperty("SmartCardAuthContext").trim();
+	}
+
+	// it cames always false by now
+	public static String isRedirectBinding() {
+		return GamSamlProperties.getInstance()
+			.getGeneralProperty("isRedirectBinding").trim();
+	}
+
+	public static String getAssertionConsumerServiceURL() {
+		String ret = GamSamlProperties.getInstance()
+			.getGeneralProperty("AssertionConsumerServiceURL").trim();
+		logger.debug("[getAssertionConsumerServiceURL] " + "Gets AssertionConsumerServiceURL: " + ret);
+		return ret;
+	}
+
+	// ATTprops
+	public static String getSAMLPARAMETER() {
+		return "SAMLResponse";
+	}
+
+	public static String getAttSamlParameter() {
+		return "SAMLPARAMETER";
+	}
+
+	public static String getAttUID() {
+		return GamSamlProperties.getInstance().getAttProperty("AttUID");
+	}
+
+	public static String getAttDOCUMENTO() {
+		return GamSamlProperties.getInstance().getAttProperty("AttDOCUMENTO");
+	}
+
+	public static String getAttPAISDOCUMENTO() {
+		return GamSamlProperties.getInstance().getAttProperty("AttPAISDOCUMENTO");
+	}
+
+	public static String getAttPRESENCIAL() {
+		return GamSamlProperties.getInstance().getAttProperty("AttPRESENCIAL");
+	}
+
+	public static String getAttTIPODOCUMENTO() {
+		return GamSamlProperties.getInstance().getAttProperty("AttTIPODOCUMENTO");
+	}
+
+	public static String getAttNOMBRECOMPLETO() {
+		return GamSamlProperties.getInstance().getAttProperty("AttNOMBRECOMPLETO");
+	}
+
+	public static String getAttCERTIFICADO() {
+		return GamSamlProperties.getInstance().getAttProperty("AttCERTIFICADO");
+	}
+
+	public static String getAttTRUE() {
+		return GamSamlProperties.getInstance().getAttProperty("AttTRUE");
+	}
+
+	public static String getAttPRIMERNOMBRE() {
+		return GamSamlProperties.getInstance().getAttProperty("AttPRIMERNOMBRE");
+	}
+
+	public static String getAttSEGUNDONOMBRE() {
+		return GamSamlProperties.getInstance().getAttProperty("AttSEGUNDONOMBRE");
+	}
+
+	public static String getAttPRIMERAPELLIDO() {
+		return GamSamlProperties.getInstance().getAttProperty("AttPRIMERAPELLIDO");
+	}
+
+	public static String getAttSEGUNDOAPELLIDO() {
+		return GamSamlProperties.getInstance().getAttProperty("AttSEGUNDOAPELLIDO");
+	}
+
+	public static String getState() {
+		return state;
+	}
+
+	private String getGeneralProperty(String prop) {
+		return getPropertyValue(generalPropsProperties, prop);
+	}
+
+	private String getAttProperty(String prop) {
+		return getPropertyValue(attPropsProperties, prop);
+	}
+
+	public String getPropertyValue(Properties config, String prop) {
+		if (config == null) {
+			return null;
+		} else {
+			return config.getProperty(prop);
+		}
+	}
+
+	public Properties loadPropsFromGAM(String state) throws Exception {
+		logger.debug("[loadPropsFromGAM] start");
+		Properties config = new Properties();
+		String[] resultString = new String[1];
+		try
+		{
+			Class<?> gamClass = Class.forName("genexus.security.api.getauthenticationparmsfromstate");
+			Object gamObj = gamClass.getDeclaredConstructor(int.class).newInstance(-1);
+			Class<?>[] paramTypes = { String.class, java.lang.String[].class };
+			Method method = gamClass.getMethod("execute", paramTypes);
+			method.invoke(gamObj, state, resultString);
+		}catch (Exception e)
+		{
+			logger.error("[loadPropsFromGAM]  reflection", e);
+		}
+		String result = resultString[0].trim();
+		logger.debug("[loadPropsFromGAM] result: " + result);
+		config.load(new StringReader(result));
+		logger.debug("[loadPropsFromGAM] config: " + config.toString());
+		return config;
+	}
+
+	public void saveConfigFile(String result) throws Exception {
+		Properties config = new Properties();
+		config.load(new StringReader(result));
+		generalPropsProperties = config;
+		logger.debug("[saveConfigFile] config: " + config.toString());
+	}
+}
---
+++ gamsaml/src/main/java/com/genexus/saml/servlet/Logout.java
@@ -0,0 +1,239 @@
+package com.genexus.saml.servlet;
+
+import com.genexus.ModelContext;
+import com.genexus.diagnostics.core.ILogger;
+import com.genexus.diagnostics.core.LogManager;
+import com.genexus.internet.HttpContext;
+import com.genexus.saml.GamSamlProperties;
+import com.genexus.saml.SamlHelper;
+import com.genexus.saml.SamlReceiver;
+import com.genexus.webpanels.HttpContextWeb;
+import org.opensaml.saml.saml2.core.LogoutRequest;
+import org.opensaml.saml.saml2.core.LogoutResponse;
+import org.opensaml.saml.saml2.core.NameID;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Method;
+
+
+public class Logout extends HttpServlet {
+	public static final ILogger logger = LogManager.getLogger(Logout.class);
+
+	private static final long serialVersionUID = -4745341452085760212L;
+	private String errorServlet;
+	private static final String SAMLPARAMETER = GamSamlProperties.getSAMLPARAMETER();
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		logger.debug("[init]");
+		super.init(config);
+		errorServlet = GamSamlProperties.getPathErrorServletSSO();
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IllegalStateException {
+
+		String samlParameter = request.getParameter(SAMLPARAMETER);
+		String alg = request.getParameter("SigAlg");
+
+		String stateParm = request.getParameter("RelayState");
+		logger.debug("[doPost] RelayState == " + stateParm);
+		GamSamlProperties props = GamSamlProperties.getInstance();
+		props.init(stateParm);
+		logger.debug("[doPost] alg =" + alg);
+
+		String signature = request.getParameter("Signature");
+		String logoutRequest = request.getParameter("SAMLRequest");
+
+		HttpSession session = request.getSession();
+
+		try {
+			if (samlParameter == null && logoutRequest == null){
+				logger.debug("[doPost] samlParameter == null");
+				try {
+					createSamlLogout(request, response);
+				} catch (Exception e) {
+					logger.error("[doPost] ", e);
+					try {
+						logYError(request,response);
+					} catch (Exception e1) {
+						logger.error("[doPost] ", e1);
+
+					}
+				}
+			} else
+			{
+				logger.debug("[LogoutResponse]");
+				logger.debug("[samlParameter]" + samlParameter);
+				//si it is a LogoutResponse
+				if (samlParameter != null){
+					SamlReceiver receiver = new SamlReceiver();
+					//get status
+					LogoutResponse resp = receiver.getLogoutResponse(samlParameter,signature,alg);
+					String identifier = (String) session.getAttribute("identifier");
+					if(resp != null)
+					{
+						String status = resp.getStatus().getStatusCode().getValue();
+						logger.debug("[status]" + status);
+						if(status.equalsIgnoreCase("urn:oasis:names:tc:SAML:2.0:status:Success") || status.equalsIgnoreCase("urn:oasis:names:tc:SAML:2.0:status:PartialLogout")){
+
+							//verifies the validity of the LogoutResponse
+							if (resp != null && !identifier.equals("") && validateLogoutResponse(resp,identifier)) {
+								logout(request,response);
+							} else {
+								try {
+									logYError(request,response);
+								} catch (Exception e1) {
+									logger.error("[doPost] ", e1);
+								}
+							}
+						}
+						else
+						{
+							String error = "&error_code="+ status + "&error_message"+ resp.getStatus().getStatusMessage().getMessage();
+							logger.error("Logout Error" + error + "  ----  " + resp.getStatus().getStatusDetail());
+						}
+					}
+					else
+					{
+						logger.error("Logout Error  ----  Response is null");
+					}
+
+				}
+				//if it is a LogoutRequest
+				if (logoutRequest != null){
+					//verifies the signature
+					SamlReceiver receiver = new SamlReceiver();
+					LogoutRequest req = receiver.getLogoutRequest(logoutRequest);
+
+					if (alg == null)
+					{
+						logger.debug("[getLogoutResponse] alg is empty");
+						alg = req.getSignature().getSignatureAlgorithm();
+						logger.debug("[getLogoutResponse] Get alg " + alg);
+					}
+
+					String sessionIndex = req.getSessionIndexes().get(0).getSessionIndex();
+					NameID nameId = req.getNameID();
+					String externalToken = nameId.getFormat() + "," + nameId.getValue();
+					externalToken += "::" + sessionIndex;
+					stateParm = "Token="+ externalToken;
+					props.updatePropsFromGAM(stateParm);
+
+					boolean isValid = receiver.validateSignature(req);
+
+					if (isValid){
+						logger.debug("[doPost] LogoutRequest with valid signature");
+						SamlHelper helper = new SamlHelper();
+						String gxcfg = getServletContext().getInitParameter("gxcfg");
+						Class contextClass = Class.forName(gxcfg);
+						com.genexus.servlet.http.IHttpServletRequest myhttpservReq  = new com.genexus.servlet.http.HttpServletRequest(request);
+						com.genexus.servlet.http.IHttpServletResponse myhttpservRes  = new com.genexus.servlet.http.HttpServletResponse(response);
+						com.genexus.servlet.IServletContext myhttpservCon  = new com.genexus.servlet.ServletContext(getServletContext());
+						HttpContext httpContext = new HttpContextWeb("POST", myhttpservReq, myhttpservRes, myhttpservCon );
+						helper.doLogoutLocalRedirect(req, response, httpContext, contextClass);
+					}
+					else
+						logger.debug("[doPost] NOT valid signature");
+				}
+			}
+		}catch (Exception e) {
+			logger.error("[doPost] ", e);
+		}
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IllegalStateException{
+		doPost(request,response);
+	}
+
+
+	private void logYError(HttpServletRequest request, HttpServletResponse response) throws IllegalStateException {
+
+		try {
+			RequestDispatcher rd = request.getRequestDispatcher(errorServlet);
+			rd.forward(request, response);
+		} catch (Exception e) {
+			logger.error("[logYError] ", e);
+		}
+	}
+
+	private void logout(HttpServletRequest request, HttpServletResponse response) throws IllegalArgumentException, SecurityException {
+		logger.debug("[logout] 200 OK");
+		//send 200 ok
+		String gxcfg = getServletContext().getInitParameter("gxcfg");
+		logger.debug("[GamSamlProperties.getState().trim()] :" + GamSamlProperties.getState().trim());
+		String token = "";
+		String fullAttributesJSON = "";
+		try {
+
+			String url = "";
+			Class contextClass = Class.forName(gxcfg);
+			com.genexus.servlet.http.IHttpServletRequest myhttpservReq  = new com.genexus.servlet.http.HttpServletRequest(request);
+			com.genexus.servlet.http.IHttpServletResponse myhttpservRes  = new com.genexus.servlet.http.HttpServletResponse(response);
+			com.genexus.servlet.IServletContext myhttpservCon  = new com.genexus.servlet.ServletContext(getServletContext());
+			HttpContext httpContext = new HttpContextWeb("POST", myhttpservReq, myhttpservRes, myhttpservCon );
+			ModelContext context = new ModelContext(contextClass);
+			context.setHttpContext(httpContext);
+			try {
+				Class<?> gamClass = Class.forName("genexus.security.api.gamexternalauthenticationinputsaml20");
+				Object gamObj = gamClass.getDeclaredConstructor(int.class, ModelContext.class).newInstance(-1, context);
+				Class<?>[] paramTypes = {String.class, String.class, String.class, String.class};
+				Method method = gamClass.getMethod("executeUdp", paramTypes);
+				url = (String) method.invoke("saml=signout",GamSamlProperties.getState().trim(), token, fullAttributesJSON);
+			} catch (Exception e) {
+				logger.error("[logout] load class with reflection ", e);
+			}
+			logger.debug("[SSO: logout] url: " + url);
+			response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+			response.setHeader("Location", url);
+		} catch (Exception e) {
+			logger.error("[logout] :", e);
+		}
+	}
+
+	private void createSamlLogout(HttpServletRequest request, HttpServletResponse response) throws IllegalArgumentException, SecurityException {
+		logger.debug("[createSamlLogout]");
+		try{
+			HttpSession session = request.getSession();
+
+			String sessionindex = request.getParameter("index");
+			String fullNameIdentifier = request.getParameter("nameid");
+			String nameIdentifier = "";
+			String nameIdentifierFormat = "";
+			if (fullNameIdentifier != null)
+			{
+				String[] splitedNameID = fullNameIdentifier.split(",");
+				nameIdentifierFormat = splitedNameID[0];
+				nameIdentifier = splitedNameID[1];
+			}
+			nameIdentifier = nameIdentifier.replace("%7c", "|");
+			logger.debug("[index]" + sessionindex);
+			SamlHelper helper = new SamlHelper();
+
+			String identifier = helper.createIdentifier();
+
+
+			session.setAttribute("identifier", identifier);
+
+
+			helper.doLogoutGlobalRedirect(response,sessionindex,identifier, nameIdentifier);
+		}
+		catch(Exception e){
+			logger.error("[createSamlLogout] ", e);
+		}
+	}
+
+	private boolean validateLogoutResponse(LogoutResponse resp, String identifier) {
+		SamlHelper helper = new SamlHelper();
+		return helper.validateLogoutResponse(resp,identifier);
+	}
+
+}
+
