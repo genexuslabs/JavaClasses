d6d1b5ba8608c9d487c20ebc986e99cef69995c6
Gonzalo Gallotti <gonzalogallotti@gmail.com>
2024-05-30 15:58:12 -0300

    Remove gxClassR dependencies

--- gxcloudstorage-ibmcos/src/main/java/com/genexus/db/driver/ExternalProviderIBM.java
+++ gxcloudstorage-ibmcos/src/main/java/com/genexus/db/driver/ExternalProviderIBM.java
@@ -1,9 +1,7 @@
 package com.genexus.db.driver;
 
-import com.genexus.Application;
 import com.genexus.StructSdtMessages_Message;
 import com.genexus.util.GXService;
-import com.genexus.util.GXServices;
 import com.genexus.util.StorageUtils;
 import com.ibm.cloud.objectstorage.ClientConfiguration;
 import com.ibm.cloud.objectstorage.HttpMethod;
@@ -56,15 +54,6 @@ public class ExternalProviderIBM extends ExternalProviderBase implements Externa
     private String folder;
     private String endpointUrl;
 	private int defaultExpirationMinutes = DEFAULT_EXPIRATION_MINUTES;
-
-    /* For compatibility reasons with GX16 U6 or lower*/
-    public ExternalProviderIBM() throws Exception {
-        this(GXServices.STORAGE_SERVICE);
-    }
-
-    public ExternalProviderIBM(String service) throws Exception {
-        this(Application.getGXServices().get(service));
-    }
 
     public ExternalProviderIBM(GXService providerService) throws Exception {
     	super(providerService);
