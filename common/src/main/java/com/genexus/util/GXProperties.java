6fe5b52874d42ea6af79946dcbb0311c58483d35
iroqueta <iroqueta@genexus.com>
2024-06-24 08:22:15 -0300

    Cherry pick branch 'genexuslabs:GXPropertiesCaseInsensitive' into beta

--- common/src/main/java/com/genexus/util/GXProperties.java
+++ common/src/main/java/com/genexus/util/GXProperties.java
@@ -10,6 +10,7 @@ import org.json.JSONException;
 import com.genexus.CommonUtil;
 import com.genexus.SdtMessages_Message;
 import com.genexus.GXBaseCollection;
+import java.util.Iterator;
 import java.util.Map;
 
 public class GXProperties implements IGxJSONSerializable {
