7df792c26e1e4ffa2e6c54f648afa6dc9d1cd178
Tomás Sexenian <99925035+tomas-sexenian@users.noreply.github.com>
2024-07-16 11:51:36 -0300

    Add missing message and fix interface return type

--- gxcompress/src/main/java/com/genexus/compression/IGXCompressor.java
+++ gxcompress/src/main/java/com/genexus/compression/IGXCompressor.java
@@ -5,8 +5,8 @@ import com.genexus.SdtMessages_Message;
 import java.util.Vector;
 
 public interface IGXCompressor {
-	static Boolean compressFiles(Vector<String> files, String path, String format, GXBaseCollection<SdtMessages_Message>[] messages) {return null;}
-	static Boolean compressFolder(String folder, String path, String format, GXBaseCollection<SdtMessages_Message>[] messages) {return null;}
+	static Boolean compressFiles(Vector<String> files, String path, String format, GXBaseCollection<SdtMessages_Message>[] messages) {return false;}
+	static Boolean compressFolder(String folder, String path, String format, GXBaseCollection<SdtMessages_Message>[] messages) {return false;}
 	static Compression newCompression(String path, String format, int dictionarySize, GXBaseCollection<SdtMessages_Message>[] messages) { return new Compression();}
-	static Boolean decompress(String file, String path, GXBaseCollection<SdtMessages_Message>[] messages) {return null;}
+	static Boolean decompress(String file, String path, GXBaseCollection<SdtMessages_Message>[] messages) {return false;}
 }
