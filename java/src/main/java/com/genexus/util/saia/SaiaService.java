c9d3a4a533336d283b27ef38a794843fd962967b
iroqueta <iroqueta@genexus.com>
2024-11-13 19:03:19 -0300

    Implement CallResult External Object to use in Agent Object

--- java/src/main/java/com/genexus/util/saia/SaiaService.java
+++ java/src/main/java/com/genexus/util/saia/SaiaService.java
@@ -1,10 +1,12 @@
 package com.genexus.util.saia;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.genexus.SdtMessages_Message;
 import com.genexus.common.interfaces.SpecificImplementation;
 import com.genexus.diagnostics.core.ILogger;
 import com.genexus.diagnostics.core.LogManager;
 import com.genexus.internet.HttpClient;
+import com.genexus.util.CallResult;
 import json.org.json.JSONObject;
 
 public class SaiaService {
@@ -12,11 +14,11 @@ public class SaiaService {
 	private static final String apiKey = (String) SpecificImplementation.Application.getProperty("AI_PROVIDER_API_KEY", "");;
 	private static final String aiProvider = (String) SpecificImplementation.Application.getProperty("AI_PROVIDER", "");
 
-	public static OpenAIResponse call(OpenAIRequest request) {
-		return call(request, false);
+	public static OpenAIResponse call(OpenAIRequest request, CallResult result) {
+		return call(request, false, result);
 	}
 
-	public static OpenAIResponse call(OpenAIRequest request, boolean isEmbedding) {
+	public static OpenAIResponse call(OpenAIRequest request, boolean isEmbedding, CallResult result) {
 		try {
 			String jsonRequest = new ObjectMapper().writeValueAsString(request);
 
@@ -33,14 +35,28 @@ public class SaiaService {
 				return new ObjectMapper().readValue(jsonResponse.toString(), OpenAIResponse.class);
 			}
 			else {
-				logger.error(String.format("Error calling Enterprise AI API, StatusCode: %d, ReasonLine: %s",
+				String errorDescription = String.format("Error calling Enterprise AI API, StatusCode: %d, ReasonLine: %s",
 					client.getStatusCode(),
-					client.getReasonLine()));
+					client.getReasonLine());
+				addResultMessage("SAIA_ERROR_CALL", (byte)1, errorDescription, result);
+				logger.error(errorDescription);
 			}
 		}
 		catch (Exception e) {
+			addResultMessage("SAIA_EXCEPTION", (byte)1, e.getMessage(), result);
 			logger.error("Calling Enterprise AI API Error", e);
 		}
 		return null;
+	}
+
+
+	private static void addResultMessage(String id, byte type, String description, CallResult result){
+		if (type == 1)
+			result.setFail();
+		SdtMessages_Message msg = new SdtMessages_Message();
+		msg.setgxTv_SdtMessages_Message_Id(id);
+		msg.setgxTv_SdtMessages_Message_Type(type);
+		msg.setgxTv_SdtMessages_Message_Description(description);
+
 	}
 }
