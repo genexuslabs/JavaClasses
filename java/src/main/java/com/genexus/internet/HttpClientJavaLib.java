878b5a2d519e29443b4a86bb8311c65595ac1d3f
Tomás Sexenian <99925035+tomas-sexenian@users.noreply.github.com>
2024-11-27 15:31:55 +0900

    Remove executor (#903)
    Issue:200786

--- java/src/main/java/com/genexus/internet/HttpClientJavaLib.java
+++ java/src/main/java/com/genexus/internet/HttpClientJavaLib.java
@@ -13,8 +13,6 @@ import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.util.*;
 import java.net.URI;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import javax.net.ssl.SSLContext;
 
 import org.apache.http.*;
@@ -95,7 +93,6 @@ public class HttpClientJavaLib extends GXHttpClient {
 	@Override
 	protected void finalize() {
 		this.closeOpenedStreams();
-		executor.shutdown();
 	}
 
 	private ConnectionKeepAliveStrategy generateKeepAliveStrategy() {
@@ -617,9 +614,7 @@ public class HttpClientJavaLib extends GXHttpClient {
 		}
 		finally {
 			if (Application.isJMXEnabled()){
-				if (executor.isShutdown())
-					executor = Executors.newSingleThreadExecutor();
-				executor.submit(this::displayHTTPConnections);
+				this.displayHTTPConnections();
 			}
 			if (getIsURL()) {
 				this.setHost(getPrevURLhost());
@@ -631,8 +626,7 @@ public class HttpClientJavaLib extends GXHttpClient {
 			resetStateAdapted();
 		}
 	}
-
-	private static ExecutorService executor = Executors.newSingleThreadExecutor();
+	
 	private synchronized void displayHTTPConnections(){
 		Iterator<HttpRoute> iterator = storedRoutes.iterator();
 		while (iterator.hasNext()) {
