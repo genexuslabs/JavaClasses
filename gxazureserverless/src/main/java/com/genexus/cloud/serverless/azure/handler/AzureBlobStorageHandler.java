08a4b8d688006208c8125283277d2e8f3e45dc2c
sjuarezgx <sjuarez@genexus.com>
2025-01-09 16:19:40 -0300

    Java Azure functions (#881)
    ---------
    
    Co-authored-by: Sabrina Juarez Garcia <sabrina.juarez@globant.com>

---
+++ gxazureserverless/src/main/java/com/genexus/cloud/serverless/azure/handler/AzureBlobStorageHandler.java
@@ -0,0 +1,57 @@
+package com.genexus.cloud.serverless.azure.handler;
+import com.genexus.cloud.serverless.model.*;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.ExecutionContext;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.*;
+
+public class AzureBlobStorageHandler extends AzureEventHandler{
+
+	EventMessages msgs = new EventMessages();
+	String rawMessage ="";
+
+	public AzureBlobStorageHandler() throws Exception {
+		super();
+	}
+	public void run(
+		@BlobTrigger(name = "content", source = "EventGrid", path = "%blob_path%", dataType = "binary") byte[] content,
+		@BindingName("name") String name,
+		final ExecutionContext context
+	) throws Exception {
+
+		context.getLogger().info("GeneXus Blob Storage trigger handler. Function processed: " + context.getFunctionName() + " Invocation Id: " + context.getInvocationId());
+		setupServerlessMappings(context.getFunctionName());
+
+		switch (executor.getMethodSignatureIdx()) {
+			case 0:
+				EventMessage msg = new EventMessage();
+				msg.setMessageId(context.getInvocationId());
+				msg.setMessageSourceType(EventMessageSourceType.BLOB);
+				Instant nowUtc = Instant.now();
+				msg.setMessageDate(Date.from(nowUtc));
+				msg.setMessageData(Base64.getEncoder().encodeToString(content));
+				List<EventMessageProperty> msgAtts = msg.getMessageProperties();
+				msgAtts.add(new EventMessageProperty("Id", context.getInvocationId()));
+				msgAtts.add(new EventMessageProperty("name", name));
+
+				msgs.add(msg);
+				break;
+			case 1:
+			case 2:
+				rawMessage = Base64.getEncoder().encodeToString(content);
+		}
+		try {
+			EventMessageResponse response = dispatchEvent(msgs, rawMessage);
+			if (response.hasFailed()) {
+				logger.error(String.format("Messages were not handled. Error: %s", response.getErrorMessage()));
+				throw new RuntimeException(response.getErrorMessage()); //Throw the exception so the runtime can Retry the operation.
+			}
+		} catch (Exception e) {
+			logger.error("HandleRequest execution error", e);
+			throw e; 		//Throw the exception so the runtime can Retry the operation.
+		}
+	}
+}
---
+++ gxazureserverless/src/main/java/com/genexus/cloud/serverless/azure/handler/AzureServiceBusTopicHandler.java
@@ -0,0 +1,36 @@
+package com.genexus.cloud.serverless.azure.handler;
+
+import com.genexus.cloud.serverless.helpers.ServiceBusBatchMessageProcessor;
+import com.genexus.cloud.serverless.helpers.ServiceBusProcessedMessage;
+import com.genexus.cloud.serverless.model.*;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.*;
+
+public class AzureServiceBusTopicHandler extends AzureEventHandler {
+
+	public AzureServiceBusTopicHandler() throws Exception {
+		super();
+	}
+
+	public void run(
+		@ServiceBusTopicTrigger(name = "messages", topicName = "%topic_name%", subscriptionName = "%subscriptionName%", connection = "%queue_connection%", cardinality = Cardinality.MANY)
+		String[] messages,
+		final ExecutionContext context
+	) throws Exception {
+
+		context.getLogger().info("GeneXus Service Bus Topic trigger handler. Function processed: " + context.getFunctionName() + " Invocation Id: " + context.getInvocationId());
+		setupServerlessMappings(context.getFunctionName());
+		ServiceBusBatchMessageProcessor queueBatchMessageProcessor = new ServiceBusBatchMessageProcessor();
+		ServiceBusProcessedMessage queueMessage = queueBatchMessageProcessor.processQueueMessage(context, executor, messages);
+		try {
+			EventMessageResponse response = dispatchEvent(queueMessage.getEventMessages(), queueMessage.getRawMessage());
+			if (response.hasFailed()) {
+				logger.error(String.format("Messages were not handled. Error: %s", response.getErrorMessage()));
+				throw new RuntimeException(response.getErrorMessage()); //Throw the exception so the runtime can Retry the operation.
+			}
+		} catch (Exception e) {
+			logger.error("HandleRequest execution error", e);
+			throw e; 		//Throw the exception so the runtime can Retry the operation.
+		}
+	}
+}
