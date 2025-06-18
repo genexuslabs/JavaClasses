5757ac42ba80727c892c178f1117b8eae8c0b3b5
mrodbratschi <mrodriguez@genexus.com>
2025-06-18 14:39:09 -0300

    A custom resolver is added to handle messages other than MapMessages to be compatible with the existing implementation of the log (the old implementation remains unchanged).

---
+++ wrappercommon/src/main/java/com/genexus/diagnostics/core/provider/CustomMessageFactory.java
@@ -0,0 +1,29 @@
+package com.genexus.diagnostics.core.provider;
+
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.layout.template.json.resolver.EventResolverContext;
+import org.apache.logging.log4j.layout.template.json.resolver.EventResolverFactory;
+import org.apache.logging.log4j.layout.template.json.resolver.TemplateResolverConfig;
+import org.apache.logging.log4j.layout.template.json.resolver.TemplateResolverFactory;
+
+
+@Plugin(name = "CustomMessageFactory", category = TemplateResolverFactory.CATEGORY)
+public final class CustomMessageFactory implements EventResolverFactory {
+	private static final CustomMessageFactory INSTANCE = new CustomMessageFactory();
+
+	@PluginFactory
+	public static CustomMessageFactory getInstance() {
+		return INSTANCE;
+	}
+
+	@Override
+	public String getName() {
+		return CustomMessageResolver.getName();
+	}
+
+	@Override
+	public CustomMessageResolver create(EventResolverContext context, TemplateResolverConfig config) {
+		return new CustomMessageResolver(config);
+	}
+}
