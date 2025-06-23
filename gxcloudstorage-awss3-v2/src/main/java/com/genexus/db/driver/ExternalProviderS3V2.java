eb39bb199bd6b164ec2518f1f537616f77f67706
tomas-sexenian <cuentastomassexenian@gmail.com>
2025-06-23 15:12:32 +0000

    Cherry pick branch 'genexuslabs:iam-s3v2' into beta

--- gxcloudstorage-awss3-v2/src/main/java/com/genexus/db/driver/ExternalProviderS3V2.java
+++ gxcloudstorage-awss3-v2/src/main/java/com/genexus/db/driver/ExternalProviderS3V2.java
@@ -121,7 +121,7 @@ public class ExternalProviderS3V2 extends ExternalProviderBase implements Extern
 			this.folder = folder;
 
 			this.client = buildS3Client(accessKey, secretKey, endpointValue, clientRegion);
-			this.presigner = buildS3Presinger(accessKey, secretKey, clientRegion);
+			this.presigner = buildS3Presigner(accessKey, secretKey, clientRegion);
 			bucketExists();
 		}
 	}
@@ -131,13 +131,13 @@ public class ExternalProviderS3V2 extends ExternalProviderBase implements Extern
 
 		boolean bUseIAM = !getPropertyValue(USE_IAM, "", "").isEmpty() || (accessKey.equals("") && secretKey.equals(""));
 
-		S3ClientBuilder builder = bUseIAM ?
-			S3Client.builder() :
-			S3Client.builder().credentialsProvider(
-				StaticCredentialsProvider.create(
-					AwsBasicCredentials.create(accessKey, secretKey)
-				)
-			);
+		S3ClientBuilder builder = bUseIAM
+			? S3Client.builder().credentialsProvider(DefaultCredentialsProvider.create())
+			: S3Client.builder().credentialsProvider(
+			StaticCredentialsProvider.create(
+				AwsBasicCredentials.create(accessKey, secretKey)
+			)
+		);
 
 		if (bUseIAM) {
 			logger.debug("Using IAM Credentials");
@@ -145,7 +145,6 @@ public class ExternalProviderS3V2 extends ExternalProviderBase implements Extern
 
 		if (!endpoint.isEmpty() && !endpoint.contains(".amazonaws.com")) {
 			pathStyleUrls = true;
-
 			s3Client = builder
 				.endpointOverride(URI.create(endpoint))
 				.region(Region.of(region))
@@ -181,11 +180,22 @@ public class ExternalProviderS3V2 extends ExternalProviderBase implements Extern
 		return s3Client;
 	}
 
-	private S3Presigner buildS3Presinger(String accessKey, String secretKey, String region) {
-		return S3Presigner.builder()
+	private S3Presigner buildS3Presigner(String accessKey, String secretKey, String region) {
+		boolean bUseIAM = !getPropertyValue(USE_IAM, "", "").isEmpty() || (accessKey.equals("") && secretKey.equals(""));
+
+		S3Presigner.Builder builder = S3Presigner.builder()
 			.region(Region.of(region))
-			.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
-			.build();
+			.credentialsProvider(
+				bUseIAM
+					? DefaultCredentialsProvider.create()
+					: StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey))
+			);
+
+		if (bUseIAM) {
+			logger.debug("Using IAM Credentials for presigner");
+		}
+
+		return builder.build();
 	}
 
 	private void bucketExists() {
