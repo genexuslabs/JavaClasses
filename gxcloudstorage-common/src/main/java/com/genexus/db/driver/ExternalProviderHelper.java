bce0ab1fca0bde702b35620d11bc406a3c30cef5
iroqueta <iroqueta@genexus.com>
2025-07-04 22:20:03 +0000

    Cherry pick branch 'genexuslabs:CreateFileFromStream' into beta

--- gxcloudstorage-common/src/main/java/com/genexus/db/driver/ExternalProviderHelper.java
+++ gxcloudstorage-common/src/main/java/com/genexus/db/driver/ExternalProviderHelper.java
@@ -3,6 +3,8 @@ package com.genexus.db.driver;
 import com.genexus.util.Encryption;
 import com.genexus.util.GXService;
 
+import java.io.*;
+
 public class ExternalProviderHelper {
 
 	public static String getServicePropertyValue(GXService s, String propName, boolean isSecure){
@@ -22,5 +24,31 @@ public class ExternalProviderHelper {
 			throw new Exception("Parameter " + name + " is required");
 		}
 		return value;
+	}
+
+	public static InputStreamWithLength getInputStreamContentLength(InputStream input) throws IOException {
+		File tempFile = File.createTempFile("upload-", ".tmp");
+		try (OutputStream out = new FileOutputStream(tempFile)) {
+			byte[] buffer = new byte[8192];
+			int bytesRead;
+			while ((bytesRead = input.read(buffer)) != -1) {
+				out.write(buffer, 0, bytesRead);
+			}
+		}
+		long size = tempFile.length();
+		InputStream newInput = new FileInputStream(tempFile);
+		return new InputStreamWithLength(newInput, size, tempFile);
+	}
+
+	public static class InputStreamWithLength {
+		public final InputStream inputStream;
+		public final long contentLength;
+		public final File tempFile; // nullable
+
+		public InputStreamWithLength(InputStream inputStream, long contentLength, File tempFile) {
+			this.inputStream = inputStream;
+			this.contentLength = contentLength;
+			this.tempFile = tempFile;
+		}
 	}
 }
