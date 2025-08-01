bca971f69eef8f808e911eda43abab0f0dccebbc
iroqueta <iroqueta@genexus.com>
2025-08-01 16:09:44 -0300

    When a GeneXus Agent was called using Chat Method the client UI was not showing de chunk returned from GEAI until all the process finish.

--- java/src/main/java/com/genexus/util/saia/SaiaService.java
+++ java/src/main/java/com/genexus/util/saia/SaiaService.java
@@ -1,15 +1,20 @@
 package com.genexus.util.saia;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.genexus.GXProcedure;
 import com.genexus.SdtMessages_Message;
 import com.genexus.common.interfaces.SpecificImplementation;
 import com.genexus.diagnostics.core.ILogger;
 import com.genexus.diagnostics.core.LogManager;
 import com.genexus.internet.HttpClient;
+import com.genexus.util.ChatResult;
+import com.genexus.util.GXProperties;
 import org.json.JSONObject;
 import com.genexus.util.CallResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
 
 public class SaiaService {
 	private static final ILogger logger = LogManager.getLogger(SaiaService.class);
@@ -17,15 +22,15 @@ public class SaiaService {
 	private static final String aiProvider = (String) SpecificImplementation.Application.getProperty("AI_PROVIDER", "");
 	private static final Logger log = LoggerFactory.getLogger(SaiaService.class);
 
-	public static OpenAIResponse call(OpenAIRequest request, HttpClient client, CallResult result) {
-		return call(request, false, client, result);
+	public static OpenAIResponse call(GXProcedure proc, OpenAIRequest request, HttpClient client, String agent, boolean stream, GXProperties properties, ArrayList<OpenAIResponse.Message> messages, CallResult result, ChatResult chatResult) {
+		return call(proc, request, false, client, agent, stream, properties, messages, result, chatResult);
 	}
 
 	public static OpenAIResponse call(OpenAIRequest request, boolean isEmbedding, CallResult result) {
-		return call(request, isEmbedding, new HttpClient(), result);
+		return call(null, request, isEmbedding, new HttpClient(), null, false, null, null, result, null);
 	}
 
-	public static OpenAIResponse call(OpenAIRequest request, boolean isEmbedding, HttpClient client, CallResult result) {
+	public static OpenAIResponse call(GXProcedure proc, OpenAIRequest request, boolean isEmbedding, HttpClient client, String agent, boolean stream, GXProperties properties, ArrayList<OpenAIResponse.Message> messages, CallResult result, ChatResult chatResult) {
 		try {
 			String jsonRequest = new ObjectMapper().writeValueAsString(request);
 			logger.debug("Agent payload: " + jsonRequest);
@@ -44,25 +49,8 @@ public class SaiaService {
 			if (client.getStatusCode() == 200) {
 				String saiaResponse;
 				if (client.getHeader("Content-Type").contains("text/event-stream")){
-					saiaResponse = client.readChunk();
-					int index = saiaResponse.indexOf("data:") + "data:".length();
-					String chunkJson = saiaResponse.substring(index).trim();
-					try {
-						JSONObject jsonResponse = new JSONObject(chunkJson);
-						OpenAIResponse chunkResponse = new ObjectMapper().readValue(jsonResponse.toString(), OpenAIResponse.class);
-						OpenAIResponse.Choice choise = chunkResponse.getChoices().get(0);
-						if (choise.getFinishReason() != null && choise.getFinishReason().equals("tool_calls")){
-							saiaResponse = chunkJson;
-						}
-						else {
-							client.unreadChunk();
-							return null;
-						}
-					}
-					catch (Exception e) {
-						client.unreadChunk();
-						return null;
-					}
+					getChunkedSaiaResponse(proc, client, agent, stream, properties, messages, result, chatResult);
+					return null;
 				}
 				else {
 					saiaResponse = client.getString();
@@ -88,6 +76,38 @@ public class SaiaService {
 		return null;
 	}
 
+	private static void getChunkedSaiaResponse(GXProcedure proc, HttpClient client, String agent, boolean stream, GXProperties properties, ArrayList<OpenAIResponse.Message> messages, CallResult result, ChatResult chatResult) {
+		String saiaChunkResponse = client.readChunk();;
+		String chunkJson;
+		while (!client.getEof()) {
+			logger.debug("Agent response chunk: " + saiaChunkResponse);
+			if (saiaChunkResponse.isEmpty() || saiaChunkResponse.equals("data: [DONE]")) {
+				saiaChunkResponse = client.readChunk();
+				continue;
+			}
+			int index = saiaChunkResponse.indexOf("data:") + "data:".length();
+			chunkJson = saiaChunkResponse.substring(index).trim();
+			try {
+				JSONObject jsonResponse = new JSONObject(chunkJson);
+				OpenAIResponse chunkResponse = new ObjectMapper().readValue(jsonResponse.toString(), OpenAIResponse.class);
+				if (!chunkResponse.getChoices().isEmpty()) {
+					OpenAIResponse.Choice choice = chunkResponse.getChoices().get(0);
+					if (choice.getFinishReason() != null && choice.getFinishReason().equals("tool_calls")) {
+						messages.add(choice.getMessage());
+						proc.processNotChunkedResponse(agent, stream, properties, messages, result, chatResult, choice.getMessage().getToolCalls());
+						;
+					} else if (choice.getDelta() != null && choice.getDelta().getContent() != null) {
+						chatResult.addChunk(((OpenAIResponse.StringContent) choice.getDelta().getContent()).getValue());
+					}
+				}
+				saiaChunkResponse = client.readChunk();
+			}
+			catch (Exception e) {
+				logger.warn("Error deserializing the response chunk", e);
+				saiaChunkResponse = client.readChunk();
+			}
+		}
+	}
 
 	private static void addResultMessage(String id, byte type, String description, CallResult result){
 		if (type == 1)
