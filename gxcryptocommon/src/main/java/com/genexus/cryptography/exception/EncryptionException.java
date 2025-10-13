469a9627618f51f69c0f31b8cd03d3bc76ebe986
iroqueta <iroqueta@genexus.com>
2025-10-13 17:32:56 -0300

    Revert this: Remove implementation of deprecated Cryptography data types Issue: 105496

---
+++ gxcryptocommon/src/main/java/com/genexus/cryptography/exception/EncryptionException.java
@@ -0,0 +1,8 @@
+package com.genexus.cryptography.exception;
+
+public class EncryptionException extends Exception {
+
+	public EncryptionException(String message, Throwable t) {
+		super(message, t);
+	}
+}
---
+++ gxcryptocommon/src/main/java/com/genexus/cryptography/exception/SignatureException.java
@@ -0,0 +1,8 @@
+package com.genexus.cryptography.exception;
+
+public class SignatureException extends Exception {
+
+	public SignatureException(Throwable t) {
+		super(t);
+	}
+}
